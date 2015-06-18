1ea060c6dcd42a5c668e59bb9522c1373c34891c
tom <tom@undev.de>
2015-06-18 09:01:16 +0200

    added files

---
+++ src/ops/ops.gl.imgeffects.js
@@ -0,0 +1,407 @@
+
+Ops.Gl=Ops.Gl || {};
+Ops.Gl.TextureEffects=Ops.Gl.TextureEffects || {};
+
+// ---------------------------------------------------------------------------------------------
+
+Ops.Gl.TextureEffects.TextureEffect = function()
+{
+    Op.apply(this, arguments);
+    var self=this;
+
+    this.name='texture effect';
+    this.render=this.addInPort(new Port(this,"render",OP_PORT_TYPE_FUNCTION));
+    this.texOut=this.addOutPort(new Port(this,"texture_out",OP_PORT_TYPE_TEXTURE));
+
+    this.tex=this.addInPort(new Port(this,"texture_in",OP_PORT_TYPE_TEXTURE));
+    this.trigger=this.addOutPort(new Port(this,"trigger",OP_PORT_TYPE_FUNCTION));
+
+    var ready=false;
+    var effect=new CGL.TextureEffect();
+
+    cgl.currentTextureEffect=effect;
+
+    this.tex.onValueChanged=function()
+    {
+        effect.setSourceTexture(self.tex.val);
+        self.texOut.val=cgl.currentTextureEffect.getCurrentSourceTexture();
+        ready=true;
+    };
+
+    this.render.onTriggered=function()
+    {
+        if(!ready)return;
+        if(!self.tex.val) return;
+        cgl.currentTextureEffect=effect;
+
+        effect.startEffect();
+        self.trigger.call();
+        self.texOut.val=cgl.currentTextureEffect.getCurrentSourceTexture();
+    };
+};
+
+Ops.Gl.TextureEffects.TextureEffect.prototype = new Op();
+
+// ---------------------------------------------------------------------------------------------
+
+Ops.Gl.TextureEffects.Invert = function()
+{
+    Op.apply(this, arguments);
+    var self=this;
+
+    this.name='Invert';
+    this.render=this.addInPort(new Port(this,"render",OP_PORT_TYPE_FUNCTION));
+    this.trigger=this.addOutPort(new Port(this,"trigger",OP_PORT_TYPE_FUNCTION));
+
+    var shader=new CGL.Shader();
+
+    var srcFrag=''
+        .endl()+'precision highp float;'
+        .endl()+'#ifdef HAS_TEXTURES'
+        .endl()+'  varying vec2 texCoord;'
+        .endl()+'  uniform sampler2D tex;'
+        .endl()+'#endif'
+        .endl()+''
+        .endl()+''
+        .endl()+'void main()'
+        .endl()+'{'
+        .endl()+'   vec4 col=vec4(1.0,0.0,0.0,1.0);'
+        .endl()+'   #ifdef HAS_TEXTURES'
+        .endl()+'       col=texture2D(tex,texCoord);'
+        .endl()+'       col.rgb=1.0-col.rgb;'
+        .endl()+'   #endif'
+        .endl()+'   gl_FragColor = col;'
+        .endl()+'}\n';
+
+    shader.setSource(shader.getDefaultVertexShader(),srcFrag);
+    var textureUniform=new CGL.Uniform(shader,'t','tex',0);
+
+
+    this.render.onTriggered=function()
+    {
+        if(!cgl.currentTextureEffect)return;
+        
+        cgl.setShader(shader);
+        cgl.currentTextureEffect.bind();
+
+        gl.activeTexture(gl.TEXTURE0);
+        gl.bindTexture(gl.TEXTURE_2D, cgl.currentTextureEffect.getCurrentSourceTexture().tex );
+
+        cgl.currentTextureEffect.finish();
+        cgl.setPreviousShader();
+
+        self.trigger.call();
+    };
+};
+
+Ops.Gl.TextureEffects.Invert.prototype = new Op();
+
+// ---------------------------------------------------------------------------------------------
+
+Ops.Gl.TextureEffects.Desaturate = function()
+{
+    Op.apply(this, arguments);
+    var self=this;
+
+    this.name='Desaturate';
+
+    this.amount=this.addInPort(new Port(this,"amount"));
+    this.render=this.addInPort(new Port(this,"render",OP_PORT_TYPE_FUNCTION));
+    this.trigger=this.addOutPort(new Port(this,"trigger",OP_PORT_TYPE_FUNCTION));
+
+    var shader=new CGL.Shader();
+
+    var srcFrag=''
+        .endl()+'precision highp float;'
+        .endl()+'#ifdef HAS_TEXTURES'
+        .endl()+'  varying vec2 texCoord;'
+        .endl()+'  uniform sampler2D tex;'
+        .endl()+'#endif'
+        .endl()+'uniform float amount;'
+        .endl()+''
+        .endl()+''
+        .endl()+'vec3 desaturate(vec3 color, float amount)'
+        .endl()+'{'
+        .endl()+'   vec3 gray = vec3(dot(vec3(0.2126,0.7152,0.0722), color));'
+        .endl()+'   return vec3(mix(color, gray, amount));'
+        .endl()+'}'
+        .endl()+''
+        .endl()+'void main()'
+        .endl()+'{'
+        .endl()+'   vec4 col=vec4(1.0,0.0,0.0,1.0);'
+        .endl()+'   #ifdef HAS_TEXTURES'
+        .endl()+'       col=texture2D(tex,texCoord);'
+        .endl()+'       col.rgb=desaturate(col.rgb,amount);'
+        .endl()+'   #endif'
+        .endl()+'   gl_FragColor = col;'
+        .endl()+'}';
+
+    shader.setSource(shader.getDefaultVertexShader(),srcFrag);
+    var textureUniform=new CGL.Uniform(shader,'t','tex',0);
+    var amountUniform=new CGL.Uniform(shader,'f','amount',1.0);
+
+    this.amount.onValueChanged=function()
+    {
+        amountUniform.setValue(self.amount.val);
+    };
+
+    this.render.onTriggered=function()
+    {
+        if(!cgl.currentTextureEffect)return;
+        
+        cgl.setShader(shader);
+        cgl.currentTextureEffect.bind();
+
+        gl.activeTexture(gl.TEXTURE0);
+        gl.bindTexture(gl.TEXTURE_2D, cgl.currentTextureEffect.getCurrentSourceTexture().tex );
+
+        cgl.currentTextureEffect.finish();
+        cgl.setPreviousShader();
+
+        self.trigger.call();
+    };
+};
+
+Ops.Gl.TextureEffects.Desaturate.prototype = new Op();
+
+// ---------------------------------------------------------------------------------------------
+
+Ops.Gl.TextureEffects.RgbMultiply = function()
+{
+    Op.apply(this, arguments);
+    var self=this;
+
+    this.name='RgbMultiply';
+
+    this.r=this.addInPort(new Port(this,"r"));
+    this.g=this.addInPort(new Port(this,"g"));
+    this.b=this.addInPort(new Port(this,"b"));
+    this.render=this.addInPort(new Port(this,"render",OP_PORT_TYPE_FUNCTION));
+    this.trigger=this.addOutPort(new Port(this,"trigger",OP_PORT_TYPE_FUNCTION));
+
+    var shader=new CGL.Shader();
+
+    var srcFrag=''
+        .endl()+'precision highp float;'
+        .endl()+'#ifdef HAS_TEXTURES'
+        .endl()+'  varying vec2 texCoord;'
+        .endl()+'  uniform sampler2D tex;'
+        .endl()+'#endif'
+        .endl()+'uniform float r;'
+        .endl()+'uniform float g;'
+        .endl()+'uniform float b;'
+        .endl()+''
+        .endl()+''
+        .endl()+'void main()'
+        .endl()+'{'
+        .endl()+'   vec4 col=vec4(1.0,0.0,0.0,1.0);'
+        .endl()+'   #ifdef HAS_TEXTURES'
+        .endl()+'       col=texture2D(tex,texCoord);'
+        .endl()+'       col.r*=r;'
+        .endl()+'       col.g*=g;'
+        .endl()+'       col.b*=b;'
+        .endl()+'   #endif'
+        .endl()+'   gl_FragColor = col;'
+        .endl()+'}';
+
+    shader.setSource(shader.getDefaultVertexShader(),srcFrag);
+    var textureUniform=new CGL.Uniform(shader,'t','tex',0);
+    var uniformR=new CGL.Uniform(shader,'f','r',1.0);
+    var uniformG=new CGL.Uniform(shader,'f','g',1.0);
+    var uniformB=new CGL.Uniform(shader,'f','b',1.0);
+
+
+    this.r.onValueChanged=function()
+    {
+        uniformR.setValue(self.r.val);
+    };
+
+    this.g.onValueChanged=function()
+    {
+        uniformG.setValue(self.g.val);
+    };
+
+    this.b.onValueChanged=function()
+    {
+        uniformB.setValue(self.b.val);
+    };
+
+    this.render.onTriggered=function()
+    {
+        if(!cgl.currentTextureEffect)return;
+        
+        cgl.setShader(shader);
+        cgl.currentTextureEffect.bind();
+
+        gl.activeTexture(gl.TEXTURE0);
+        gl.bindTexture(gl.TEXTURE_2D, cgl.currentTextureEffect.getCurrentSourceTexture().tex );
+
+        cgl.currentTextureEffect.finish();
+        cgl.setPreviousShader();
+
+        self.trigger.call();
+    };
+};
+
+Ops.Gl.TextureEffects.RgbMultiply.prototype = new Op();
+
+// ---------------------------------------------------------------------------------------------
+
+Ops.Gl.TextureEffects.Vignette = function()
+{
+    Op.apply(this, arguments);
+    var self=this;
+
+    this.name='Vignette';
+
+    this.lensRadius1=this.addInPort(new Port(this,"lensRadius1"));
+    this.lensRadius2=this.addInPort(new Port(this,"lensRadius2"));
+    this.render=this.addInPort(new Port(this,"render",OP_PORT_TYPE_FUNCTION));
+    this.trigger=this.addOutPort(new Port(this,"trigger",OP_PORT_TYPE_FUNCTION));
+
+    var shader=new CGL.Shader();
+
+    var srcFrag=''
+        .endl()+'precision highp float;'
+        .endl()+'#ifdef HAS_TEXTURES'
+        .endl()+'  varying vec2 texCoord;'
+        .endl()+'  uniform sampler2D tex;'
+        .endl()+'#endif'
+        .endl()+'uniform float lensRadius1;'
+        .endl()+'uniform float lensRadius2;'
+        .endl()+''
+        .endl()+''
+        .endl()+'void main()'
+        .endl()+'{'
+        .endl()+'   vec4 col=vec4(1.0,0.0,0.0,1.0);'
+        .endl()+'   #ifdef HAS_TEXTURES'
+        .endl()+'       col=texture2D(tex,texCoord);'
+        .endl()+'       float dist = distance(texCoord, vec2(0.5,0.5));'
+        .endl()+'       col.rgb *= smoothstep(lensRadius1, lensRadius2, dist);'
+        .endl()+'   #endif'
+        .endl()+'   gl_FragColor = col;'
+        .endl()+'}';
+
+    shader.setSource(shader.getDefaultVertexShader(),srcFrag);
+    var textureUniform=new CGL.Uniform(shader,'t','tex',0);
+    var uniLensRadius1=new CGL.Uniform(shader,'f','lensRadius1',0.4);
+    var uniLensRadius2=new CGL.Uniform(shader,'f','lensRadius2',0.3);
+
+    this.lensRadius1.onValueChanged=function()
+    {
+        uniLensRadius1.setValue(self.lensRadius1.val);
+    };
+
+    this.lensRadius2.onValueChanged=function()
+    {
+        uniLensRadius2.setValue(self.lensRadius2.val);
+    };
+
+    this.lensRadius1.val=0.8;
+    this.lensRadius2.val=0.4;
+
+    this.render.onTriggered=function()
+    {
+        if(!cgl.currentTextureEffect)return;
+        
+        cgl.setShader(shader);
+        cgl.currentTextureEffect.bind();
+
+        gl.activeTexture(gl.TEXTURE0);
+        gl.bindTexture(gl.TEXTURE_2D, cgl.currentTextureEffect.getCurrentSourceTexture().tex );
+
+        cgl.currentTextureEffect.finish();
+        cgl.setPreviousShader();
+
+        self.trigger.call();
+    };
+};
+
+Ops.Gl.TextureEffects.Vignette.prototype = new Op();
+
+// ---------------------------------------------------------------------------------------------
+
+Ops.Gl.TextureEffects.Blur = function()
+{
+    Op.apply(this, arguments);
+    var self=this;
+
+    this.name='Blur';
+    this.render=this.addInPort(new Port(this,"render",OP_PORT_TYPE_FUNCTION));
+    this.trigger=this.addOutPort(new Port(this,"trigger",OP_PORT_TYPE_FUNCTION));
+
+    var shader=new CGL.Shader();
+
+    var srcFrag=''
+        .endl()+'precision highp float;'
+        .endl()+'#ifdef HAS_TEXTURES'
+        .endl()+'  varying vec2 texCoord;'
+        .endl()+'  uniform sampler2D tex;'
+        .endl()+'  uniform float dirX;'
+        .endl()+'  uniform float dirY;'
+        .endl()+'#endif'
+        .endl()+''
+        .endl()+'vec4 blur9(sampler2D texture, vec2 uv, vec2 red, vec2 dir)'
+        .endl()+'{'
+        .endl()+'   vec4 color = vec4(0.0);'
+        .endl()+'   vec2 offset1 = vec2(1.3846153846) * dir;'
+        .endl()+'   vec2 offset2 = vec2(3.2307692308) * dir;'
+        .endl()+'   color += texture2D(texture, uv) * 0.2270270270;'
+        .endl()+'   color += texture2D(texture, uv + (offset1 / red)) * 0.3162162162;'
+        .endl()+'   color += texture2D(texture, uv - (offset1 / red)) * 0.3162162162;'
+        .endl()+'   color += texture2D(texture, uv + (offset2 / red)) * 0.0702702703;'
+        .endl()+'   color += texture2D(texture, uv - (offset2 / red)) * 0.0702702703;'
+        .endl()+'   return color;'
+        .endl()+'}'
+        .endl()+''
+        .endl()+'void main()'
+        .endl()+'{'
+        .endl()+'   vec4 col=vec4(1.0,0.0,0.0,1.0);'
+        .endl()+'   #ifdef HAS_TEXTURES'
+        .endl()+'       col=blur9(tex,texCoord,vec2(512.0,512.0),vec2(dirX,dirY));'
+        // .endl()+ '       col=blur9(tex,texCoord,vec2(512.0,512.0),vec2(dirX*1.4,dirY*1.4));'
+        .endl()+'   #endif'
+        .endl()+'   gl_FragColor = col;'
+        .endl()+'}\n';
+
+    shader.setSource(shader.getDefaultVertexShader(),srcFrag);
+    var textureUniform=new CGL.Uniform(shader,'t','tex',0);
+    var uniDirX=new CGL.Uniform(shader,'f','dirX',0);
+    var uniDirY=new CGL.Uniform(shader,'f','dirY',0);
+
+    this.render.onTriggered=function()
+    {
+        if(!cgl.currentTextureEffect)return;
+        cgl.setShader(shader);
+
+        // first pass
+
+        cgl.currentTextureEffect.bind();
+        gl.activeTexture(gl.TEXTURE0);
+        gl.bindTexture(gl.TEXTURE_2D, cgl.currentTextureEffect.getCurrentSourceTexture().tex );
+
+        uniDirX.setValue(0.0);
+        uniDirY.setValue(1.0);
+
+        cgl.currentTextureEffect.finish();
+
+        // second pass
+
+        cgl.currentTextureEffect.bind();
+        gl.activeTexture(gl.TEXTURE0);
+        gl.bindTexture(gl.TEXTURE_2D, cgl.currentTextureEffect.getCurrentSourceTexture().tex );
+
+        uniDirX.setValue(1.0);
+        uniDirY.setValue(0.0);
+
+        cgl.currentTextureEffect.finish();
+        
+        cgl.setPreviousShader();
+
+        self.trigger.call();
+    };
+};
+
+Ops.Gl.TextureEffects.Blur.prototype = new Op();
+
+
---
+++ src/ops/ops.gl.meshes.js
@@ -0,0 +1,360 @@
+
+Ops.Gl.Meshes=Ops.Gl.Meshes || {};
+
+// --------------------------------------------------------------------------
+
+Ops.Gl.Meshes.Triangle = function()
+{
+    Op.apply(this, arguments);
+    var self=this;
+
+    this.name='Triangle';
+    this.render=this.addInPort(new Port(this,"render",OP_PORT_TYPE_FUNCTION));
+    this.trigger=this.addOutPort(new Port(this,"trigger",OP_PORT_TYPE_FUNCTION));
+
+    this.render.onTriggered=function()
+    {
+        self.mesh.render(cgl.getShader());
+        self.trigger.call();
+    };
+
+    var geom=new CGL.Geometry();
+    geom.vertices = [
+         0.0,  1.0,  0.0,
+        -1.0,  -1.0,  0.0,
+         1.0, -1.0,  0.0
+    ];
+
+    geom.verticesIndices = [
+        0, 1, 2
+    ];
+    this.mesh=new CGL.Mesh(geom);
+};
+
+Ops.Gl.Meshes.Triangle.prototype = new Op();
+
+// --------------------------------------------------------------------------
+
+Ops.Gl.Meshes.Rectangle = function()
+{
+    Op.apply(this, arguments);
+    var self=this;
+
+    this.name='rectangle';
+    this.render=this.addInPort(new Port(this,"render",OP_PORT_TYPE_FUNCTION));
+    this.trigger=this.addOutPort(new Port(this,"trigger",OP_PORT_TYPE_FUNCTION));
+    this.width=this.addInPort(new Port(this,"width"));
+    this.height=this.addInPort(new Port(this,"width"));
+    this.width.val=1.0;
+    this.height.val=1.0;
+
+    this.render.onTriggered=function()
+    {
+        self.mesh.render(cgl.getShader());
+        self.trigger.call();
+    };
+
+    var geom=new CGL.Geometry();
+    this.mesh=null;
+
+    function rebuild()
+    {
+        geom.vertices = [
+             self.width.val/2,  self.height.val/2,  0.0,
+            -self.width.val/2,  self.height.val/2,  0.0,
+             self.width.val/2, -self.height.val/2,  0.0,
+            -self.width.val/2, -self.height.val/2,  0.0
+        ];
+
+        geom.texCoords = [
+             1.0, 1.0,
+             0.0, 1.0,
+             1.0, 0.0,
+             0.0, 0.0
+        ];
+
+        geom.verticesIndices = [
+            0, 1, 2,
+            3, 1, 2
+        ];
+        if(!self.mesh) self.mesh=new CGL.Mesh(geom);
+        self.mesh.setGeom(geom);
+        console.log('builkd geo');
+        
+    }
+    rebuild();
+
+    this.width.onValueChanged=rebuild;
+    this.height.onValueChanged=rebuild;
+};
+
+Ops.Gl.Meshes.Rectangle.prototype = new Op();
+
+// --------------------------------------------------------------------------
+
+Ops.Gl.Meshes.FullscreenRectangle = function()
+{
+    Op.apply(this, arguments);
+    var self=this;
+
+    this.name='fullscreen rectangle';
+    this.render=this.addInPort(new Port(this,"render",OP_PORT_TYPE_FUNCTION));
+    this.trigger=this.addOutPort(new Port(this,"trigger",OP_PORT_TYPE_FUNCTION));
+
+    this.render.onTriggered=function()
+    {
+        cgl.pushPMatrix();
+        mat4.identity(cgl.pMatrix);
+
+        cgl.pushMvMatrix();
+        mat4.identity(cgl.mvMatrix);
+
+        self.mesh.render(cgl.getShader());
+        self.trigger.call();
+
+        cgl.popPMatrix();
+        cgl.popMvMatrix();
+
+    };
+
+    var geom=new CGL.Geometry();
+    geom.vertices = [
+         1.0,  1.0,  0.0,
+        -1.0,  1.0,  0.0,
+         1.0, -1.0,  0.0,
+        -1.0, -1.0,  0.0
+    ];
+
+    geom.texCoords = [
+         1.0, 1.0,
+         0.0, 1.0,
+         1.0, 0.0,
+         0.0, 0.0
+    ];
+
+    geom.verticesIndices = [
+        0, 1, 2,
+        3, 1, 2
+    ];
+    this.mesh=new CGL.Mesh(geom);
+};
+
+Ops.Gl.Meshes.FullscreenRectangle.prototype = new Op();
+
+// --------------------------------------------------------------------------
+
+Ops.Gl.Meshes.Circle = function()
+{
+    Op.apply(this, arguments);
+    var self=this;
+
+    this.name='Circle';
+    this.render=this.addInPort(new Port(this,"render",OP_PORT_TYPE_FUNCTION));
+
+    this.segments=this.addInPort(new Port(this,"segments"));
+    this.radius=this.addInPort(new Port(this,"radius"));
+    this.percent=this.addInPort(new Port(this,"percent"));
+
+    this.trigger=this.addOutPort(new Port(this,"trigger",OP_PORT_TYPE_FUNCTION));
+
+    this.render.onTriggered=function()
+    {
+        mesh.render(cgl.getShader());
+        self.trigger.call();
+    };
+
+    this.segments.val=20;
+    this.radius.val=1;
+    this.percent.val=1;
+
+    var geom=new CGL.Geometry();
+    var mesh=new CGL.Mesh(geom);
+
+    function calc()
+    {
+        geom.clear();
+        var oldPosX=0;
+        var oldPosY=0;
+
+        for (var i=0; i <= self.segments.val*self.percent.val; i++)
+        {
+            var degInRad = (360/self.segments.val)*i*CGL.DEG2RAD;
+            var posx=Math.cos(degInRad)*self.radius.val;
+            var posy=Math.sin(degInRad)*self.radius.val;
+
+            geom.addFace(
+                        [posx,posy,0],
+                        [oldPosX,oldPosY,0],
+                        [0,0,0]
+                        );
+
+            geom.texCoords.push(0,0,0,0,0,0);
+
+            oldPosX=posx;
+            oldPosY=posy;
+        }
+
+        mesh.setGeom(geom);
+    }
+
+    this.segments.onValueChanged=calc;
+    this.radius.onValueChanged=calc;
+    this.percent.onValueChanged=calc;
+};
+
+Ops.Gl.Meshes.Circle.prototype = new Op();
+
+// --------------------------------------------------------------------------
+
+Ops.Gl.Meshes.ObjMesh = function()
+{
+    Op.apply(this, arguments);
+    var self=this;
+
+    this.name='OBJ Mesh';
+    this.render=this.addInPort(new Port(this,"render",OP_PORT_TYPE_FUNCTION));
+    this.trigger=this.addOutPort(new Port(this,"trigger",OP_PORT_TYPE_FUNCTION));
+
+    this.mesh=null;
+
+    this.render.onTriggered=function()
+    {
+        if(self.mesh) self.mesh.render(cgl.getShader());
+
+        self.trigger.call();
+    };
+
+    ajaxRequest('assets/skull.obj',function(response)
+    {
+        console.log(response);
+                
+        var r=parseOBJ(response);
+
+    unwrap = function(ind, crd, cpi)
+    {
+        var ncrd = new Array(Math.floor(ind.length/3)*cpi);
+        for(var i=0; i<ind.length; i++)
+        {
+            for(var j=0; j<cpi; j++)
+            {
+                ncrd[i*cpi+j] = crd[ind[i]*cpi+j];
+            }
+        }
+        return ncrd;
+    };
+
+    var l=r.verticesIndices.length;
+        r.vertices = unwrap(r.verticesIndices, r.vertices, 3);
+        r.texCoords = unwrap(r.texCoordsIndices  , r.texCoords  , 2);
+        r.verticesIndices = [];
+        for(var i=0; i<l; i++) r.verticesIndices.push(i);
+        
+        self.mesh=new CGL.Mesh(r);
+    });
+};
+
+Ops.Gl.Meshes.ObjMesh.prototype = new Op();
+
+// ----------------------------------------------------------------
+
+Ops.Gl.Meshes.Cube = function()
+{
+    Op.apply(this, arguments);
+    var self=this;
+
+    this.name='Cube';
+    this.render=this.addInPort(new Port(this,"render",OP_PORT_TYPE_FUNCTION));
+    this.trigger=this.addOutPort(new Port(this,"trigger",OP_PORT_TYPE_FUNCTION));
+
+    this.mesh=null;
+
+    this.render.onTriggered=function()
+    {
+        if(self.mesh!==null) self.mesh.render(cgl.getShader());
+        self.trigger.call();
+    };
+
+    var geom=new CGL.Geometry();
+
+            geom.vertices = [
+            // Front face
+            -1.0, -1.0,  1.0,
+             1.0, -1.0,  1.0,
+             1.0,  1.0,  1.0,
+            -1.0,  1.0,  1.0,
+            // Back face
+            -1.0, -1.0, -1.0,
+            -1.0,  1.0, -1.0,
+             1.0,  1.0, -1.0,
+             1.0, -1.0, -1.0,
+            // Top face
+            -1.0,  1.0, -1.0,
+            -1.0,  1.0,  1.0,
+             1.0,  1.0,  1.0,
+             1.0,  1.0, -1.0,
+            // Bottom face
+            -1.0, -1.0, -1.0,
+             1.0, -1.0, -1.0,
+             1.0, -1.0,  1.0,
+            -1.0, -1.0,  1.0,
+            // Right face
+             1.0, -1.0, -1.0,
+             1.0,  1.0, -1.0,
+             1.0,  1.0,  1.0,
+             1.0, -1.0,  1.0,
+            // Left face
+            -1.0, -1.0, -1.0,
+            -1.0, -1.0,  1.0,
+            -1.0,  1.0,  1.0,
+            -1.0,  1.0, -1.0,
+        ];
+
+        geom.texCoords = [
+          // Front face
+          0.0, 0.0,
+          1.0, 0.0,
+          1.0, 1.0,
+          0.0, 1.0,
+          // Back face
+          1.0, 0.0,
+          1.0, 1.0,
+          0.0, 1.0,
+          0.0, 0.0,
+          // Top face
+          0.0, 1.0,
+          0.0, 0.0,
+          1.0, 0.0,
+          1.0, 1.0,
+          // Bottom face
+          1.0, 1.0,
+          0.0, 1.0,
+          0.0, 0.0,
+          1.0, 0.0,
+          // Right face
+          1.0, 0.0,
+          1.0, 1.0,
+          0.0, 1.0,
+          0.0, 0.0,
+          // Left face
+          0.0, 0.0,
+          1.0, 0.0,
+          1.0, 1.0,
+          0.0, 1.0,
+        ];
+
+        geom.verticesIndices = [
+            0, 1, 2,      0, 2, 3,    // Front face
+            4, 5, 6,      4, 6, 7,    // Back face
+            8, 9, 10,     8, 10, 11,  // Top face
+            12, 13, 14,   12, 14, 15, // Bottom face
+            16, 17, 18,   16, 18, 19, // Right face
+            20, 21, 22,   20, 22, 23  // Left face
+        ];
+
+    this.mesh=new CGL.Mesh(geom);
+};
+
+Ops.Gl.Meshes.Cube.prototype = new Op();
+
+// ----------------------------------------------------------------
+
