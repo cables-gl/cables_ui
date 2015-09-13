8a25eac4bc5b05221187bbaed8c8a2db69668aa2
tom <tom@undev.de>
2015-09-13 15:45:39 +0200

    deleting files

--- src/ops/ops.math.js
+++ src/ops/ops.math.js
@@ -210,29 +210,52 @@ Ops.Math.SmoothStep = function()
 
     this.name='SmoothStep';
     this.result=this.addOutPort(new Port(this,"result"));
-    this.number=this.addInPort(new Port(this,"number1"));
-    this.max=this.addInPort(new Port(this,"min"));
-    this.min=this.addInPort(new Port(this,"max"));
+    this.number=this.addInPort(new Port(this,"number"));
+    this.min=this.addInPort(new Port(this,"min"));
+    this.max=this.addInPort(new Port(this,"max"));
+
+    var min=0;
+    var max=1;
+    var subAdd=0;
 
     this.exec= function()
     {
-        // todo make it work with negative min value ?
-        
-        var x = Math.max(0, Math.min(1, (self.number.val-self.min.val)/(self.max.val-self.min.val)));
-        self.result.val= x*x*(3 - 2*x);
+        var val=self.number.val;
+
+        // todo negative min ?
+
+        var x = Math.max(0, Math.min(1, (val-min)/(max-min)));
+        self.result.val= x*x*(3 - 2*x); // smoothstep
+        // return x*x*x*(x*(x*6 - 15) + 10); // smootherstep
+
     };
 
-    this.number.onValueChanged=this.exec;
-    this.max.onValueChanged=this.exec;
-    this.min.onValueChanged=this.exec;
-
-    this.number.val=0.5;
     this.min.val=0;
     this.max.val=1;
+    this.number.val=0;
+
+    function setValues()
+    {
+        min=self.min.val;
+        max=self.max.val;
+
+        // if(min<0)
+        // {
+        //     subAdd=min*-1;
+        //     min+=subAdd;
+        //     max+=subAdd;
+        // }
+        // else subAdd=0;
+    }
+
+    this.number.onValueChanged=this.exec;
+    this.max.onValueChanged=setValues;
+    this.min.onValueChanged=setValues;
+
+    setValues();
 };
 
 Ops.Math.SmoothStep.prototype = new Op();
-
 
 // ---------------------------------------------------------------------------
 
