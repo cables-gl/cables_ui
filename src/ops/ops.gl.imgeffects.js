5f0a04bd4ee4d0ff8de587cfe0ff0282714600cd
tom <tom@undev.de>
2015-09-27 22:57:29 +0200

    noise

--- src/ops/ops.gl.imgeffects.js
+++ src/ops/ops.gl.imgeffects.js
@@ -2317,3 +2317,100 @@ Ops.Gl.TextureEffects.FXAA = function()
 Ops.Gl.TextureEffects.FXAA.prototype = new Op();
 
 
+
+
+
+
+
+
+
+// ---------------------------------------------------------------------------------------------
+
+Ops.Gl.TextureEffects.Noise = function()
+{
+    Op.apply(this, arguments);
+    var self=this;
+
+    this.name='Noise';
+    this.render=this.addInPort(new Port(this,"render",OP_PORT_TYPE_FUNCTION));
+    this.trigger=this.addOutPort(new Port(this,"trigger",OP_PORT_TYPE_FUNCTION));
+
+    this.amount=this.addInPort(new Port(this,"amount",OP_PORT_TYPE_VALUE,{display:'range'}));
+
+    var shader=new CGL.Shader();
+
+    var srcFrag=''
+        .endl()+'precision highp float;'
+        .endl()+'#ifdef HAS_TEXTURES'
+        .endl()+'  varying vec2 texCoord;'
+        .endl()+'  uniform sampler2D tex;'
+        .endl()+'#endif'
+        .endl()+''
+        .endl()+'uniform float amount;'
+
+        .endl()+'uniform float time;'
+        .endl()+''
+        .endl()+'float random(vec2 co)'
+        .endl()+'{'
+        .endl()+'   return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453);'
+        .endl()+'}'
+        .endl()+'void main()'
+        .endl()+'{'
+        .endl()+'   float c=random(time*texCoord.xy);'
+
+
+        .endl()+'vec4 col=texture2D(tex,texCoord);'
+
+        .endl()+'col.rgb=mix(col.rgb,vec3(c),amount);'
+
+        .endl()+'   gl_FragColor = col;'
+        .endl()+'}';
+
+        // .endl()+''
+        // .endl()+'void main()'
+        // .endl()+'{'
+        // .endl()+'   vec4 col=vec4(0.0,0.0,0.0,1.0);'
+        // .endl()+'   #ifdef HAS_TEXTURES'
+        // .endl()+'       col=texture2D(tex,vec2(mod(texCoord.x+amountX*0.1,1.0),mod(texCoord.y+amountY*0.1,1.0) ));'
+        // .endl()+'   #endif'
+        // .endl()+'   gl_FragColor = col;'
+        // .endl()+'}\n';
+
+    shader.setSource(shader.getDefaultVertexShader(),srcFrag);
+    var textureUniform=new CGL.Uniform(shader,'t','tex',0);
+
+    this.render.onTriggered=function()
+    {
+        if(!cgl.currentTextureEffect)return;
+        
+        timeUniform.setValue(self.patch.timer.getTime());
+
+        cgl.setShader(shader);
+        cgl.currentTextureEffect.bind();
+
+        cgl.gl.activeTexture(cgl.gl.TEXTURE0);
+        cgl.gl.bindTexture(cgl.gl.TEXTURE_2D, cgl.currentTextureEffect.getCurrentSourceTexture().tex );
+
+        cgl.currentTextureEffect.finish();
+        cgl.setPreviousShader();
+
+        self.trigger.trigger();
+    };
+
+    var amountUniform=new CGL.Uniform(shader,'f','amount',1.0);
+    var timeUniform=new CGL.Uniform(shader,'f','time',1.0);
+
+    this.amount.onValueChanged=function()
+    {
+        amountUniform.setValue(self.amount.val);
+    };
+
+    this.amount.val=0.3;
+
+
+};
+
+Ops.Gl.TextureEffects.Noise.prototype = new Op();
+
+// ---------------------------------------------------------------------------------------------
+
