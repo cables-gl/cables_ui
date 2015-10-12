948e061932469730a43d7e212e7480bcbf17fe27
tom <tom@undev.de>
2015-10-12 18:23:15 +0200

    readpixel op

--- src/ops/ops.gl.js
+++ src/ops/ops.gl.js
@@ -435,6 +435,7 @@ Ops.Gl.ColorPick=function()
     var self=this;
 
     this.name='ColorPick';
+    this.render=this.addInPort(new Port(this,"render",OP_PORT_TYPE_FUNCTION));
     this.x=this.addInPort(new Port(this,"x",OP_PORT_TYPE_VALUE));
     this.y=this.addInPort(new Port(this,"y",OP_PORT_TYPE_VALUE));
 
@@ -444,21 +445,24 @@ Ops.Gl.ColorPick=function()
     this.a=this.addOutPort(new Port(this,"a",OP_PORT_TYPE_VALUE));
 
     var pixelValues = new Uint8Array(4);
-    
-    this.render=this.addInPort(new Port(this,"render",OP_PORT_TYPE_FUNCTION));
-    this.render.onTriggered=function()
+
+    function render()
     {
         cgl.gl.readPixels(self.x.val, self.y.val, 1,1,  cgl.gl.RGBA, cgl.gl.UNSIGNED_BYTE ,pixelValues);
-        console.log('buff',pixelValues);
         self.r.val=pixelValues[0]/255;
         self.g.val=pixelValues[1]/255;
         self.b.val=pixelValues[2]/255;
         self.a.val=pixelValues[3]/255;
-    };
+    }
+    
+    this.render.onTriggered=render;
 
 };
 
 Ops.Gl.ColorPick.prototype = new Op();
+Ops.Gl.ReadPixel=Ops.Gl.ColorPick;
+
+
 // --------------------------------------------------------------------------
 
 
