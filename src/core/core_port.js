a08b0cb729fa50ad563246522afa8e4c93c7587b
tom <tom@undev.de>
2015-12-06 15:25:11 +0100

    exception handler

--- src/core/core_port.js
+++ src/core/core_port.js
@@ -122,8 +122,8 @@ CABLES.Port=function(parent,name,type,uiAttribs)
                         this.crashed=true;
                         this.setValue=function(v){};
                         this.onTriggered=function(){};
-                        console.error('onvaluechanged exception cought',ex);
-                        console.error('exception in: '+parent.name);
+                        console.log('onvaluechanged exception cought',ex);
+                        console.log('exception in: '+parent.name);
                     }
                 }
 
