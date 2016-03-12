cda122cb4416f661281ebf6664ca9f0b095bccec
tom <tom@undev.de>
2016-03-12 10:01:34 +0100

    info area texts

---
+++ src/ui/text.js
@@ -0,0 +1,24 @@
+CABLES=CABLES || {};
+CABLES.UI=CABLES.UI || {};
+CABLES.UI.TEXTS=
+{
+    linkAddCircle:"## Link \n\n* left click: insert op \n* right click: delete link",
+
+    timeLineToggle:"toggle timeline / keyframing panel",
+
+    portFloatDrag:"* hold left mouse button and drag to change value. \n\n* use shift to for smaller increase.",
+    portUnlink:"unlink port",
+    portCreateOp:"create fitting op",
+    portAnimKeep:"keep in keyframing view",
+    portAnimToggle:"toggle parameter animation. enable to keyframe parameter",
+
+    portDirIn:'## Input Port \n\n',
+    portDirOut:'## Output Port\n\n',
+    portMouseUnlink:'* press right mouse button to unlink port',
+    portMouseCreate:'* press left mousebutton (or press and drag) to create and link new op',
+
+    projectSettingsPublic:"make project public - everyone can see it",
+    projectSettingsTitle:"project title - click to edit",
+    projectSettingsExample:"ADMIN: project will be listed as example"
+
+};
