ff813298f3c2c01e21b4347e8c25ad86b69bda30
tom <tom@undev.de>
2016-03-12 10:51:45 +0100

    background job progress display

---
+++ src/ui/jobs.js
@@ -0,0 +1,50 @@
+CABLES=CABLES || {};
+CABLES.UI=CABLES.UI || {};
+CABLES.UI.Jobs=CABLES.UI.Jobs ||
+
+function()
+{
+    var jobs=[];
+
+    function updateJobListing()
+    {
+        var str='';
+
+        for(var i in jobs)
+        {
+            str+='<li><i class="fa fa-circle-o-notch fa-spin"></i>&nbsp;&nbsp;'+jobs[i].title+'</li>';
+        }
+
+        $('#jobs').html(str);
+    }
+
+
+    this.start=function(job)
+    {
+        $('.cables .logo').removeClass('cablesLogo');
+        $('.cables .logo').addClass('fa fa-circle-o-notch fa-spin');
+
+        jobs.push(job);
+        updateJobListing();
+    };
+
+    this.finish=function(jobId)
+    {
+        for(var i in jobs)
+        {
+            if(jobs[i].id==jobId)
+            {
+                jobs.splice(i,1);
+                break;
+            }
+        }
+
+        if(jobs.length===0)
+        {
+            $('.cables .logo').addClass('cablesLogo');
+            $('.cables .logo').removeClass('fa fa-circle-o-notch fa-spin');
+        }
+        updateJobListing();
+
+    };
+};
