32d1a3d073ef72d251f6887eecbeb74a6c2e8526
tom <tom@undev.de>
2016-06-15 12:43:47 +0200

    suggestions anim

--- src/ui/suggestions.js
+++ src/ui/suggestions.js
@@ -47,26 +47,52 @@ CABLES.UI.SuggestionDialog=function(op,portname,mouseEvent,coords,cb)
 
     var sugDegree=6;
     var sugHeight=20;
-    for(var i=0;i<suggestions.length;i++)
+    var i=0;
+
+    for(i=0;i<suggestions.length;i++)
     {
         suggestions[i].id=i;
         suggestions[i].rot=(((i)-(suggestions.length/2)) * sugDegree);
-        suggestions[i].left=30-Math.abs(((i)-((suggestions.length-1)/2))*6);
+        suggestions[i].left=15-Math.abs(((i)-((suggestions.length-1)/2))*3)/2;
         suggestions[i].top=(i*sugHeight)-(suggestions.length*sugHeight/2)-sugHeight;
         suggestions[i].shortName=suggestions[i].name.substr(4,suggestions[i].name.length);
     }
 
     var html = CABLES.UI.getHandleBarHtml('suggestions',{suggestions: suggestions });
     $('#suggestionDialog').html(html);
+    $('#modalbg').show();
     $('#suggestionDialog').show();
-
     $('#suggestionDialog').css(
     {
         left:mouseEvent.clientX,
         top:mouseEvent.clientY,
     });
 
-    $('#modalbg').show();
+
+    $( ".opSelect" ).css({width:5,height:5,  "margin-left":2.5,"margin-top":-2.5});
+    $( ".opSelect" ).animate({
+      width:30,
+      height:30,
+      "margin-left":-15,
+      "margin-top":-15
+    }, 100);
+
+    for(i=0;i<suggestions.length;i++)
+    {
+        suggestions[i].rot=(((i)-(suggestions.length/2)) * sugDegree);
+        var left=15-Math.abs(((i)-((suggestions.length-1)/2))*3);
+
+        suggestions[i].shortName=suggestions[i].name.substr(4,suggestions[i].name.length);
+
+        $( "#suggestion"+i ).css({opacity:0});
+
+        $( "#suggestion"+i ).delay( Math.abs(i-suggestions.length/2) *25).animate({
+          opacity: 1,
+          left: left,
+      }, 230);
+
+        suggestions[i].id=i;
+    }
 
     $('#modalbg').on('click',function(){
         self.close();
