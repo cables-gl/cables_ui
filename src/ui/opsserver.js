fe04e2c0d0e528fcac8a445764cf8bdb5c44c778
Tim Pulver <git@timpulver.de>
2017-04-12 18:07:24 +0200

    Fixes #188
    Special characters in summary are now removed before send to server

--- src/ui/opsserver.js
+++ src/ui/opsserver.js
@@ -224,6 +224,7 @@ CABLES.UI.ServerOps=function(gui)
 
     this.addOpSummary=function(opName,summary)
     {
+        if(summary) summary = summary.replace(/[^a-z0-9]/gi, ' '); // remove non-url-save characters
         CABLES.api.put(
             'op/'+opName+'/summary/'+summary,
             function(res)
