bcd913fb0115c61c409e8a3835374bb31cdaa205
tom <tom@undev.de>
2017-07-29 23:51:38 +0200

    vars

--- src/ui/meta_variables.js
+++ src/ui/meta_variables.js
@@ -4,9 +4,25 @@ CABLES.UI=CABLES.UI || {};
 
 CABLES.UI.Variables=function()
 {
-
     this._lastTimeout=0;
+};
 
+CABLES.UI.Variables.prototype.update=function()
+{
+    console.log(CABLES.UI.userSettings.get("metatab"));
+    if(CABLES.UI.userSettings.get("metatab")=='variables')
+    {
+        clearTimeout(this._lastTimeout);
+
+        var vars=gui.patch().scene.getVars();
+        for(var i in vars)
+        {
+            $('#varval'+i).html(vars[i].getValue());
+        }
+
+        this._lastTimeout=setTimeout(this.update.bind(this),100);
+
+    }
 };
 
 CABLES.UI.Variables.prototype.show=function()
@@ -17,7 +33,6 @@ CABLES.UI.Variables.prototype.show=function()
     });
     $('#meta_content_variables').html(html);
 
-    clearTimeout(this._lastTimeout);
-    if(CABLES.UI.userSettings.get("metatab")=='variables') this._lastTimeout=setTimeout(this.show.bind(this),300);
+    this.update();
 
 };
