c900515db7828f75bfbbd11ce4104487987e16e0
Tim Pulver <git@timpulver.de>
2017-08-30 12:52:09 +0200

    Remove cookie-warning in editor (vue)

--- vue-src/App.vue
+++ vue-src/App.vue
@@ -2,21 +2,18 @@
   <div class="vue">
     <sidebar></sidebar>
     <sidebar-customizer></sidebar-customizer>
-    <cookie-warning></cookie-warning>
   </div>
 </template>
 
 <script>
 import Sidebar from './components/Sidebar.vue'
 import SidebarCustomizer from './components/SidebarCustomizer.vue'
-import CookieWarning from './components/CookieWarning.vue'
 
 export default {
   name: 'app',
   components: {
     Sidebar,
     SidebarCustomizer,
-    CookieWarning,
   }
 }
 </script>
