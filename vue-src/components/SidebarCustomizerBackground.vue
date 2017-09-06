6cb3be473daa6e9dd08d6e398f104c318765c9f6
Tim Pulver <git@timpulver.de>
2017-09-06 14:22:44 +0200

    Add sidebar-customizer-background

---
+++ vue-src/components/SidebarCustomizerBackground.vue
@@ -0,0 +1,16 @@
+<template>
+    <transition name="sidebar-customizer-bg-fade">
+        <div v-if="visible" class="sidebar-customizer-background"></div>
+    </transition>
+</template>
+
+<script>
+export default {
+    name: 'sidebar-customizer-background',
+    computed: {
+        visible() {
+            return this.$store.state.sidebar.customizerVisible;
+            },
+    },
+};
+</script>
