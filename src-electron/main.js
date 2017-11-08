854c979135a0eee2284499fc3e4ec7234cba0826
Tim Pulver <git@timpulver.de>
2017-11-08 17:07:38 +0100

    Clean up

--- src-electron/main.js
+++ src-electron/main.js
@@ -1,10 +1,5 @@
 const electron = require('electron');
-// Module to control application life.
-const app = electron.app;
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow;
-
-const { Menu, dialog } = electron;
+const { app, BrowserWindow, Menu, dialog, shell } = electron;
 
 const fs = require('fs');
 const path = require('path');
