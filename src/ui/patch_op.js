5d09f36c5ca5cea733cee2095e14b59fc6622e16
pandrr <tom@drastic.net>
2019-06-07 10:02:26 +0200

    fix contextmenu windows

--- src/ui/patch_op.js
+++ src/ui/patch_op.js
@@ -320,26 +320,24 @@ var OpRect = function(_opui, _x, _y, _w, _h, _text, objName) {
         gui.setStateUnsaved();
     };
 
-    var up = function(e) {
+    var up = function(e)
+    {
         this._updateElementOrder(false);
 
-        if ( (e.metaKey || e.altKey) && CABLES.UI.quickAddOpStart) {
-            gui.patch().linkTwoOps(
-                CABLES.UI.quickAddOpStart,
-                CABLES.UI.selectedEndOp
-            );
+        if ( (e.metaKey || e.altKey) && CABLES.UI.quickAddOpStart)
+        {
+            gui.patch().linkTwoOps(CABLES.UI.quickAddOpStart,CABLES.UI.selectedEndOp);
 
             CABLES.UI.quickAddOpStart = null;
             CABLES.UI.selectedEndOp = null;
 
-            return;
+            e.stopPropagation();
+            e.preventDefault();
+            return false;
         }
-
 
         shakeCountP = 0;
         shakeCountN = 0;
-
-        // lastX = -1;
 
         if (CABLES.UI.LINKHOVER)
         {
@@ -353,9 +351,7 @@ var OpRect = function(_opui, _x, _y, _w, _h, _text, objName) {
                     portOut = oldLink.p1;
                 }
 
-                // portIn.thePort.removeLinks();
                 oldLink.unlink();
-                
 
                 if (CABLES.Link.canLink(opui.op.portsIn[0], portOut.thePort)) {
                     gui.patch().scene.link(
