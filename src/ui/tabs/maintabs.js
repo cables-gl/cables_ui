7ce44c795f628a8fa833fec749110039044e2a43
pandrr <tom@drastic.net>
2019-07-19 13:54:43 +0200

    tabs/editor

---
+++ src/ui/tabs/maintabs.js
@@ -0,0 +1,66 @@
+CABLES =CABLES || {};
+CABLES.UI =CABLES.UI || {};
+
+CABLES.UI.MainTabPanel=function(tabs)
+{
+    this._tabs=tabs;
+    this._visible=false;
+    this._ele=document.getElementById("maintabs");
+    this._ele.style.display="none";
+    this._tabs.addTab(new CABLES.UI.Tab("welcome",{"icon":"cables","closable":true}));
+
+    this._tabs.addEventListener("onTabAdded",function(tab)
+    {
+        var wasVisible=this._visible;
+        if(!wasVisible) this.show();
+        
+        tabs.activateTab(tab.id);
+        
+        if(!wasVisible && window.gui) gui.setLayout();
+
+    }.bind(this));
+
+    this._tabs.addEventListener("onTabRemoved",function(tab)
+    {
+        if(this._tabs.getNumTabs()==0)
+        {
+            this.hide();
+            gui.setLayout();
+        }
+    }.bind(this));
+};
+
+CABLES.UI.MainTabPanel.prototype.init=function()
+{
+    console.log(CABLES.UI.userSettings.get("maintabsVisible"));
+    if(CABLES.UI.userSettings.get("maintabsVisible"))
+        this.show();
+}
+
+CABLES.UI.MainTabPanel.prototype.isVisible=function()
+{
+    return this._visible;
+}
+
+CABLES.UI.MainTabPanel.prototype.show=function()
+{
+    this._visible=true;
+    this._ele.style.display="block";
+    CABLES.UI.userSettings.set("maintabsVisible",true);
+
+    console.log(CABLES.UI.userSettings.get("maintabsVisible"));
+}
+
+CABLES.UI.MainTabPanel.prototype.hide=function()
+{
+    this._visible=false;
+    this._ele.style.display="none";
+    if(window.gui)gui.setLayout();
+    CABLES.UI.userSettings.set("maintabsVisible",false);
+}
+
+CABLES.UI.MainTabPanel.prototype.toggle=function()
+{
+    if(this._visible) this.hide();
+        else this.show();
+}
