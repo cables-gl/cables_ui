64d7836a67d307578fded6bfac2040eb28837b18
pandrr <tom@undev.de>
2020-03-23 09:32:22 +0100

    cablesidlemode

---
+++ src/ui/idlemode.js
@@ -0,0 +1,62 @@
+CABLES = CABLES || {};
+CABLES.UI = CABLES.UI || {};
+
+CABLES.UI.idling=false;
+CABLES.UI.idleTimeout=null;
+CABLES.UI.idleTime=60;
+
+CABLES.UI.startIdleMode=function()
+{
+    if(CABLES.UI.idling)return;
+
+
+    if(!document.hidden && CABLES.UI.idleFocus)
+    {
+        console.log("is focussed, not starting idle mode!!");
+        return;
+    }
+
+    // console.log("start idle...");
+    // console.log("document.hidden:",document.hidden);
+    // console.log("CABLES.UI.idleFocus:",CABLES.UI.idleFocus);
+    
+    CABLES.UI.MODAL.show('<center><b>cables is paused!</b><br/>click to resume<br/></center>');
+
+    gui.patch().scene.pause();
+    CABLES.UI.idling=true;
+    clearTimeout(CABLES.UI.idleTimeout);
+}
+
+CABLES.UI.stopIdleMode=function()
+{
+    if(!CABLES.UI.idling) return;
+
+    gui.patch().scene.resume();
+    CABLES.UI.MODAL.hide();
+    CABLES.UI.idling=false;
+    clearTimeout(CABLES.UI.idleTimeout);
+}
+
+CABLES.UI.visibilityChanged=function(e)
+{
+    CABLES.UI.idleTimeout=clearTimeout(CABLES.UI.idleTimeout);
+    if(document.hidden) setTimeout(CABLES.UI.startIdleMode,5000);
+    else CABLES.UI.stopIdleMode();
+}
+
+window.addEventListener('focus', (event) => {
+    CABLES.UI.idleFocus=true;
+    clearTimeout(CABLES.UI.idleTimeout);
+    CABLES.UI.stopIdleMode();
+});
+
+window.addEventListener('blur', (event) => {
+    CABLES.UI.idleFocus=false;
+    clearTimeout(CABLES.UI.idleTimeout);
+    CABLES.UI.idleTimeout=setTimeout(CABLES.UI.startIdleMode,CABLES.UI.idleTime*1000);
+});
+
+CABLES.UI.idleTimeout=setTimeout(CABLES.UI.startIdleMode,CABLES.UI.idleTime*1000);
+
+document.addEventListener("visibilitychange", CABLES.UI.visibilityChanged);
+
