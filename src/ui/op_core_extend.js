2a30a75468dce77955093259c4cd1f90f9a04f18
pandrr <tom@undev.de>
2020-05-03 09:11:16 +0200

    move ui core stuff to ui repo

---
+++ src/ui/op_core_extend.js
@@ -0,0 +1,74 @@
+    // needs to be in UI only
+    CABLES.Op.prototype.checkLinkTimeWarnings = function ()
+    {
+        function hasParent(op, type, name)
+        {
+            for (var i = 0; i < op.portsIn.length; i++)
+            {
+                if (op.portsIn[i].type == type && op.portsIn[i].isLinked())
+                {
+                    var pi = op.portsIn[i];
+                    for (var li = 0; li < pi.links.length; li++)
+                    {
+                        if (!pi.links[li]) continue;
+                        if (pi.links[li].portOut.parent.objName.indexOf(name) > -1) return true;
+                        if (hasParent(pi.links[li].portOut.parent, type, name)) return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        function hasTriggerInput(op)
+        {
+            if (op.portsIn.length > 0 && op.portsIn[0].type == CONSTANTS.OP.OP_PORT_TYPE_FUNCTION) return true;
+            return false;
+        }
+
+        var notWorkingMsg = null;
+        var working = true;
+
+        if (working && this.objName.indexOf("Ops.Gl.TextureEffects") == 0 && hasTriggerInput(this) && this.objName.indexOf("TextureEffects.ImageCompose") == -1)
+        {
+            working = hasParent(this, CONSTANTS.OP.OP_PORT_TYPE_FUNCTION, "TextureEffects.ImageCompose");
+            if (!working) notWorkingMsg = CABLES.UI.TEXTS.working_connected_to + "ImageCompose";
+        }
+
+        if (this._needsParentOp && working)
+        {
+            working = hasParent(this, CONSTANTS.OP.OP_PORT_TYPE_OBJECT, this._needsParentOp);
+            if (!working) notWorkingMsg = CABLES.UI.TEXTS.working_connected_to + this._needsParentOp;
+        }
+
+        if (this._needsLinkedToWork.length > 0)
+        {
+            for (var i = 0; i < this._needsLinkedToWork.length; i++)
+            {
+                var p = this._needsLinkedToWork[i];
+                if (!p)
+                {
+                    console.warn("[needsLinkedToWork] port not found");
+                    continue;
+                }
+                if (!p.isLinked())
+                {
+                    working = false;
+
+                    if (!notWorkingMsg) notWorkingMsg = CABLES.UI.TEXTS.working_connected_needs_connections_to;
+                    else notWorkingMsg += ", ";
+                    notWorkingMsg += p.name.toUpperCase();
+                }
+            }
+        }
+
+        if (!working)
+        {
+            this.setUiAttrib({ working,notWorkingMsg:notWorkingMsg });
+            this.setUiError("notworking",notWorkingMsg,1);
+        }
+        else if (!this.uiAttribs.working)
+        {
+            this.setUiAttrib({ working: true,notWorkingMsg:null });
+            this.setUiError("notworking",null);
+        }
+    };
