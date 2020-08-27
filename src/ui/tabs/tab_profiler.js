bd099191073823c96bd1ec0cb2fe273d6f8a3cac
pandrr <tom@undev.de>
2020-08-27 09:19:34 +0200

    profiler subtabs

--- src/ui/tabs/tab_profiler.js
+++ src/ui/tabs/tab_profiler.js
@@ -10,7 +10,23 @@ CABLES.UI.Profiler = function (tabs)
     this.colors = ["#7AC4E0", "#D183BF", "#9091D6", "#FFC395", "#F0D165", "#63A8E8", "#CF5D9D", "#66C984", "#D66AA6", "#515151"];
     this.intervalId = null;
     this.lastPortTriggers = 0;
+    this._subTab = 0;
 };
+
+CABLES.UI.Profiler.prototype.setTab = function (which)
+{
+    document.getElementById("profilerTabOps").classList.remove("tabActiveSubtab");
+    document.getElementById("profilerTabSubpatches").classList.remove("tabActiveSubtab");
+    document.getElementById("profilerTabPeaks").classList.remove("tabActiveSubtab");
+    if (which == 0) document.getElementById("profilerTabOps").classList.add("tabActiveSubtab");
+    if (which == 1) document.getElementById("profilerTabSubpatches").classList.add("tabActiveSubtab");
+    if (which == 2) document.getElementById("profilerTabPeaks").classList.add("tabActiveSubtab");
+
+    gui.corePatch().profiler.clear();
+    this._subTab = which;
+    this.update();
+};
+
 
 CABLES.UI.Profiler.prototype.show = function ()
 {
@@ -105,38 +121,43 @@ CABLES.UI.Profiler.prototype.update = function ()
     let item = null;
     let pad = "";
 
-    html += "<table>";
-
-    for (let i in sortedItems)
+    if (this._subTab == 0)
     {
-        item = sortedItems[i];
-        pad = "";
+        html += "<h3>Ops</h3>";
+        html += "<table>";
 
-        html += "<tr><td>";
-        if (sortedItems.length > 0)
-            for (i = 0; i < 2 - (item.percent + "").length; i++)
-                pad += "&nbsp;";
-
-        html += pad + Math.floor(item.percent * 100) / 100 + "% </td><td>" + item.title + "</td><td> " + item.numTriggers + " triggers</td><td> " + Math.round(item.timeUsed) + "ms </td>";
-        if (item.numCumulated)html += "<td>" + item.numCumulated + " ops</td>";
-        html += "</tr>";
-
-        if (item.percent > 0)
+        for (let i in sortedItems)
         {
-            htmlBar += "<div class=\"tt\" data-tt=\"" + item.title + "\" style=\"height:20px;background-color:" + this.colors[colorCounter] + ";float:left;padding:0px;overflow:hidden;min-width:0px;width:" + item.percent + "%\"></div>";
-            colorCounter++;
-            if (colorCounter > this.colors.length - 1)colorCounter = 0;
+            item = sortedItems[i];
+            pad = "";
+
+            html += "<tr><td>";
+            if (sortedItems.length > 0)
+                for (i = 0; i < 2 - (item.percent + "").length; i++)
+                    pad += "&nbsp;";
+
+            html += pad + Math.floor(item.percent * 100) / 100 + "% </td><td>" + item.title + "</td><td> " + item.numTriggers + " triggers</td><td> " + Math.round(item.timeUsed) + "ms </td>";
+            if (item.numCumulated)html += "<td>" + item.numCumulated + " ops</td>";
+            html += "</tr>";
+
+            if (item.percent > 0)
+            {
+                htmlBar += "<div class=\"tt\" data-tt=\"" + item.title + "\" style=\"height:20px;background-color:" + this.colors[colorCounter] + ";float:left;padding:0px;overflow:hidden;min-width:0px;width:" + item.percent + "%\"></div>";
+                colorCounter++;
+                if (colorCounter > this.colors.length - 1)colorCounter = 0;
+            }
         }
+
+        html += "</table>";
     }
 
-    html += "</table>";
-
     // peak list
-    let htmlPeaks = "";
+    const htmlPeaks = "";
     sortedItems.sort(function (a, b) { return b.peak - a.peak; });
 
 
-    if (Object.keys(cumulatedSubPatches).length > 1)
+    // if (Object.keys(cumulatedSubPatches).length > 1)
+    if (this._subTab == 1)
     {
         const subPatches = [];
         let allTimesUsed = 0;
@@ -165,17 +186,21 @@ CABLES.UI.Profiler.prototype.update = function ()
     }
 
 
-    for (let i in sortedItems)
+    if (this._subTab == 2)
     {
-        item = sortedItems[i];
-        pad = "";
+        html += "<h3>Peaks</h3>";
+        for (let i in sortedItems)
+        {
+            item = sortedItems[i];
+            pad = "";
 
-        if (sortedItems.length > 0) for (i = 0; i < 2 - (item.peak + "").length; i++)pad += "&nbsp;";
-        htmlPeaks += pad + (Math.round(96 * item.peak) / 100) + "ms " + item.title + "<br/>";
+            if (sortedItems.length > 0) for (i = 0; i < 2 - (item.peak + "").length; i++)pad += "&nbsp;";
+            html += pad + (Math.round(96 * item.peak) / 100) + "ms " + item.title + "<br/>";
+        }
     }
 
     document.getElementById("profilerui").style.display = "block";
-    document.getElementById("profilerlistPeaks").innerHTML = htmlPeaks;
+    // document.getElementById("profilerlistPeaks").innerHTML = htmlPeaks;
     document.getElementById("profilerbar").innerHTML = htmlBar;
     document.getElementById("profilerlist").innerHTML = html;
     document.getElementById("profilerstartbutton").style.display = "none";
@@ -184,5 +209,10 @@ CABLES.UI.Profiler.prototype.update = function ()
 CABLES.UI.Profiler.prototype.start = function ()
 {
     gui.corePatch().profile(true);
+    this.update();
+    document.getElementById("profilerTabOps").addEventListener("click", () => { this.setTab(0); });
+    document.getElementById("profilerTabSubpatches").addEventListener("click", () => { this.setTab(1); });
+    document.getElementById("profilerTabPeaks").addEventListener("click", () => { this.setTab(2); });
+
     if (!this.intervalId) this.intervalId = setInterval(this.update.bind(this), 1000);
 };
