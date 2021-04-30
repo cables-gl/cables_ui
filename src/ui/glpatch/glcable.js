8a1ea6120a43510c29e061e5510fcd2ffca637ac
tom <tom@undev.de>
2021-04-30 15:47:48 +0200

    only upload lines when changed

--- src/ui/glpatch/glcable.js
+++ src/ui/glpatch/glcable.js
@@ -64,7 +64,7 @@ CABLES.GLGUI.GlCable = class
     _subdivivde(inPoints)
     {
         const arr = [];
-        const subd = 6;
+        const subd = 4;
         let newLen = (inPoints.length - 4) * (subd - 1);
 
         if (newLen != arr.length) arr.length = Math.floor(Math.abs(newLen));
@@ -139,7 +139,7 @@ CABLES.GLGUI.GlCable = class
                         this._x, this._y, 0,
                         this._x, this._y - (Math.abs(distY) * 0.002) - 15, 0,
 
-                        (this._x + this._x2) * 0.5, (this._y + this._y2) * 0.5 - (0.005 * distY), 0,
+                        (this._x + this._x2) * 0.5, (this._y + this._y2) * 0.5, 0, // * 0.5 - (0.001 * distY), 0,
 
                         this._x2, this._y2 + (Math.abs(distY) * 0.002) + 15, 0,
                         this._x2, this._y2, 0,
