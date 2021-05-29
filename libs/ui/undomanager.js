547d84d2abb5db9a76dffa153e55f3fc0fbe2c7e
tom <tom@undev.de>
2021-05-29 13:44:21 +0200

    pause undomanager

--- libs/ui/undomanager.js
+++ libs/ui/undomanager.js
@@ -3,28 +3,31 @@ Simple Javascript undo and redo.
 https://github.com/ArthurClemens/Javascript-Undo-Manager
 */
 
-;(function() {
-
-	'use strict';
-
-    function removeFromTo(array, from, to) {
+(function ()
+{
+    function removeFromTo(array, from, to)
+    {
         array.splice(from,
             !to ||
             1 + to - from + (!(to < 0 ^ from >= 0) && (to < 0 || -1) * array.length));
         return array.length;
     }
 
-    var UndoManager = function() {
-        var commands = [],
+    let UndoManager = function ()
+    {
+        let commands = [],
             index = -1,
             groupIndex = 0,
             limit = 0,
             isExecuting = false,
             callback,
+            paused = false,
             execute;
 
-        execute = function(command, action) {
-            if (!command || typeof command[action] !== "function") {
+        execute = function (command, action)
+        {
+            if (!command || typeof command[action] !== "function")
+            {
                 return this;
             }
             isExecuting = true;
@@ -40,8 +43,12 @@ https://github.com/ArthurClemens/Javascript-Undo-Manager
             /*
             Add a command to the queue.
             */
-            add: function (command) {
-                if (isExecuting) {
+            "add": function (command)
+            {
+                if (paused) return;
+
+                if (isExecuting)
+                {
                     return this;
                 }
                 // if we are here after having called undo,
@@ -51,13 +58,15 @@ https://github.com/ArthurClemens/Javascript-Undo-Manager
                 commands.push(command);
 
                 // if limit is set, remove items from the start
-                if (limit && commands.length > limit) {
-                    removeFromTo(commands, 0, -(limit+1));
+                if (limit && commands.length > limit)
+                {
+                    removeFromTo(commands, 0, -(limit + 1));
                 }
 
                 // set the current index to the end
                 index = commands.length - 1;
-                if (callback) {
+                if (callback)
+                {
                     callback();
                 }
 
@@ -67,26 +76,33 @@ https://github.com/ArthurClemens/Javascript-Undo-Manager
             /*
             Pass a function to be called on undo and redo actions.
             */
-            setCallback: function (callbackFunc) {
+            "setCallback": function (callbackFunc)
+            {
                 callback = callbackFunc;
             },
 
             /*
             Perform undo: call the undo function at the current index and decrease the index by 1.
             */
-            undo: function () {
-                var command = commands[index];
-                if (!command) {
+            "undo": function ()
+            {
+                if (paused) return;
+
+                let command = commands[index];
+                if (!command)
+                {
                     return this;
                 }
-              var g = command.group;
-              while(command.group === g){
-                execute(command, "undo");
-                index -= 1;
-                command = commands[index];
-                if(!command || !command.group) break;
-              }
-                if (callback) {
+                let g = command.group;
+                while (command.group === g)
+                {
+                    execute(command, "undo");
+                    index -= 1;
+                    command = commands[index];
+                    if (!command || !command.group) break;
+                }
+                if (callback)
+                {
                     callback();
                 }
 
@@ -96,101 +112,134 @@ https://github.com/ArthurClemens/Javascript-Undo-Manager
             /*
             Perform redo: call the redo function at the next index and increase the index by 1.
             */
-            redo: function () {
-                var command = commands[index + 1];
-                if (!command) {
+            "redo": function ()
+            {
+                if (paused) return;
+
+                let command = commands[index + 1];
+                if (!command)
+                {
                     return this;
                 }
-              var g = command.group;
-              while(command.group === g){
-                execute(command, "redo");
-                index += 1;
-                command = commands[index+1];
-                if(!command || !command.group) break;
-              }
-                if (callback) {
+                let g = command.group;
+                while (command.group === g)
+                {
+                    execute(command, "redo");
+                    index += 1;
+                    command = commands[index + 1];
+                    if (!command || !command.group) break;
+                }
+                if (callback)
+                {
                     callback();
                 }
                 return this;
             },
 
-          group: function(step, name,idx){
-            if(!step) return groupIndex;
-            idx = idx || index;
-            if(step<1) step = 1;
-            groupIndex += 1;
-            while(step-- && idx-step>=0)
-                commands[idx-step].group = groupIndex;
+            "group": function (step, name, idx)
+            {
+                if (paused) return;
 
-            commands[index].groupName=name;
-            return groupIndex;
-          },
+                if (!step) return groupIndex;
+                idx = idx || index;
+                if (step < 1) step = 1;
+                groupIndex += 1;
+                while (step-- && idx - step >= 0)
+                    commands[idx - step].group = groupIndex;
+
+                commands[index].groupName = name;
+                return groupIndex;
+            },
 
             /*
             Clears the memory, losing all stored states. Reset the index.
             */
-            clear: function () {
-                var prev_size = commands.length;
+            "clear": function ()
+            {
+                let prev_size = commands.length;
 
                 commands = [];
                 index = -1;
                 groupIndex = 0;
 
-                if (callback && (prev_size > 0)) {
+                if (callback && (prev_size > 0))
+                {
                     callback();
                 }
             },
 
-            hasUndo: function () {
+            "hasUndo": function ()
+            {
                 return index !== -1;
             },
 
-            hasRedo: function () {
+            "hasRedo": function ()
+            {
                 return index < (commands.length - 1);
             },
 
-            getCommands: function () {
+            "getCommands": function ()
+            {
                 return commands;
             },
 
-            getIndex: function() {
-                return index;
-            },
-            startGroup: function() {
-                return index;
-            },
-            endGroup:function(group,name)
+            "getIndex": function ()
             {
-                this.group(index-group,name);
-                if (callback) {
+                return index;
+            },
+            "startGroup": function ()
+            {
+                return index;
+            },
+            "endGroup": function (group, name)
+            {
+                this.group(index - group, name);
+                if (callback)
+                {
                     callback();
                 }
-
             },
 
-          getGroup: function(g) {
-            var G = [];
-            for(var i = 0, len = commands.length; i < len; i++) {
-              if( commands[i].group === g ) G.push( {index:i, command:commands[i]} );
-            }
-            return G;
-          },
+            "getGroup": function (g)
+            {
+                let G = [];
+                for (let i = 0, len = commands.length; i < len; i++)
+                {
+                    if (commands[i].group === g) G.push({ "index": i, "command": commands[i] });
+                }
+                return G;
+            },
 
-            setLimit: function (l) {
+            "setLimit": function (l)
+            {
                 limit = l;
-            }
+            },
+
+            "pause": function ()
+            {
+                paused = true;
+            },
+            "resume": function ()
+            {
+                paused = false;
+            },
         };
     };
 
-	if (typeof define === 'function' && typeof define.amd === 'object' && define.amd) {
-		// AMD. Register as an anonymous module.
-		define(function() {
-			return UndoManager;
-		});
-	} else if (typeof module !== 'undefined' && module.exports) {
-		module.exports = UndoManager;
-	} else {
-		window.UndoManager = UndoManager;
-	}
-
+    if (typeof define === "function" && typeof define.amd === "object" && define.amd)
+    {
+        // AMD. Register as an anonymous module.
+        define(function ()
+        {
+            return UndoManager;
+        });
+    }
+    else if (typeof module !== "undefined" && module.exports)
+    {
+        module.exports = UndoManager;
+    }
+    else
+    {
+        window.UndoManager = UndoManager;
+    }
 }());
