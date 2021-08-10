e9b4046fdb8a0f2c4944bb85e36cd945b69f2ba9
pandrr <tom+github@undev.de>
2021-08-10 13:24:49 +0200

    fix

--- src/ui/commands/cmd_debug.js
+++ src/ui/commands/cmd_debug.js
@@ -37,6 +37,15 @@ CABLES.CMD.DEBUG.debugGlUi = function ()
     gui.maintabPanel.show();
 };
 
+CABLES.CMD.DEBUG.buildInfo = function ()
+{
+    // const uiBuild = CABLESUILOADER.buildInfo.ui;
+    // const coreBuild = CABLESUILOADER.buildInfo.core;
+    // const apiBuild = CABLESUILOADER.buildInfo.api;
+
+    CABLES.UI.MODAL.show("<code>" + JSON.stringify(CABLESUILOADER.buildInfo, false, 4) + "</code>");
+};
+
 
 CABLES.CMD.commands.push(
     {
@@ -67,6 +76,12 @@ CABLES.CMD.commands.push(
         "cmd": "glgui tab",
         "category": "debug",
         "func": CABLES.CMD.DEBUG.glguiTab,
+        "icon": "command"
+    },
+    {
+        "cmd": "build info",
+        "category": "debug",
+        "func": CABLES.CMD.DEBUG.buildInfo,
         "icon": "command"
     }
 
