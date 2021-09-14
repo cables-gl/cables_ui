7d2e166ec8cd6dde73989611eee3e0ff5bc8a440
pandrr <tom+github@undev.de>
2021-09-14 16:26:18 +0200

    fix close tab looses op focus

--- src/ui/glpatch/glpatch.js
+++ src/ui/glpatch/glpatch.js
@@ -345,10 +345,20 @@ CABLES.GLGUI.GlPatch = class extends CABLES.EventTarget
 
         this.emitEvent("mousedown", e);
         this._rectInstancer.mouseDown(e);
+
+        console.log("canvas mousedown!");
+        this._canvasMouseDown = true;
     }
 
     _onCanvasMouseUp(e)
     {
+        if (!this._canvasMouseDown)
+        {
+            console.log("cancel mouse up!");
+            return;
+        }
+        this._canvasMouseDown = false;
+
         this._removeDropInRect();
         this._rectInstancer.mouseUp(e);
 
