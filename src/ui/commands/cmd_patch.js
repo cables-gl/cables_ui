9be80ad413d6a313455c6b6ec2ce1ff2ceaab2ff
pandrr <tom+github@undev.de>
2021-09-28 15:28:27 +0200

    fix

--- src/ui/commands/cmd_patch.js
+++ src/ui/commands/cmd_patch.js
@@ -38,8 +38,8 @@ CABLES_CMD_PATCH.save = function (force)
                 dosave = false;
 
                 CABLES.UI.MODAL.showError("Not Collaborator", "You are not a collaborator of this patch<br/>Be sure the owner knows that you make changes to this patch...<br/><br/>");
-                CABLES.UI.MODAL.contentElement.innerHTML += "<a class=\"button\" onclick=\"CABLES.UI.MODAL.hide(true);CABLES.sandbox.addMeUserlist(null,()=>{CABLES_CMD_PATCH.save(true);});\">Add me as collaborator and save</a>&nbsp;&nbsp;";
-                CABLES.UI.MODAL.contentElement.innerHTML += "<a class=\"button\" onclick=\"CABLES.UI.MODAL.hide(true);CABLES_CMD_PATCH.save(true);\">Save anyway</a>&nbsp;&nbsp;";
+                CABLES.UI.MODAL.contentElement.innerHTML += "<a class=\"button\" onclick=\"CABLES.UI.MODAL.hide(true);CABLES.sandbox.addMeUserlist(null,()=>{CABLES.CMD.PATCH.save(true);});\">Add me as collaborator and save</a>&nbsp;&nbsp;";
+                CABLES.UI.MODAL.contentElement.innerHTML += "<a class=\"button\" onclick=\"CABLES.UI.MODAL.hide(true);CABLES.CMD.PATCH.save(true);\">Save anyway</a>&nbsp;&nbsp;";
                 CABLES.UI.MODAL.contentElement.innerHTML += "<a class=\"button\" onclick=\"CABLES.UI.MODAL.hide(true);\">Close</a>&nbsp;&nbsp;";
             }
         }
