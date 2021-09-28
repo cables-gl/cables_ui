a83e3bcc77b4c416efcb280263bec7eb9236d7d9
pandrr <tom+github@undev.de>
2021-09-28 16:44:20 +0200

    uisavepatch event

--- src/ui/api/patchServerApi.js
+++ src/ui/api/patchServerApi.js
@@ -303,7 +303,7 @@ export default class PatchSaveServer extends CABLES.EventTarget
             document.getElementById("patchname").innerHTML = "Saving Patch";
             document.getElementById("patchname").classList.add("blinking");
 
-            gui.corePatch.emitEvent("uiSavePatch");
+            gui.corePatch().emitEvent("uiSavePatch");
 
             console.log("saving data ", Math.round(uint8data.length / 1024) + " / " + origSize + "kb");
 
