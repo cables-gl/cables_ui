1ac564d2584715834bb37cef82ae3d1a2331b5b9
pandrr <tom+github@undev.de>
2021-09-30 17:07:34 +0200

    fix remoteviewer

--- src/ui/components/nopatcheditor.js
+++ src/ui/components/nopatcheditor.js
@@ -3,7 +3,14 @@ export default class NoPatchEditor extends CABLES.EventTarget
     constructor(cgl)
     {
         super();
+        gui.patchView.setPatchRenderer(null, this);
     }
 
     get name() { return "nopatch"; }
+
+    setSize() {}
+
+    setProject() {}
+
+    getCurrentSubPatch() { return 0; }
 }
