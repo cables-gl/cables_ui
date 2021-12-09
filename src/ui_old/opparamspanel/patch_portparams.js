e7a47e82c95cc44d521aa3814d997a6ed5fb270c
pandrr <tom+github@undev.de>
2021-12-09 09:35:23 +0100

    scroll bug

--- src/ui_old/opparamspanel/patch_portparams.js
+++ src/ui_old/opparamspanel/patch_portparams.js
@@ -21,6 +21,7 @@ CABLES.UI.inputListenerCursorKeys = function (e)
 
 CABLES.UI.inputListenerMousewheel = function (event)
 {
+    event.preventDefault();
     let delta = -event.deltaY;
     if (ele.hasFocus(event.target))
     {
