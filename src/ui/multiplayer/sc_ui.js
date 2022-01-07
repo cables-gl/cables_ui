334ca37bd091b1acdad6e085b33cc20890ac081f
Stephan Maihöfer <stephan@undev.de>
2022-01-07 13:14:16 +0100

    demote sc-errors to logging-events for now, fix missing import for LoggingTab

--- src/ui/multiplayer/sc_ui.js
+++ src/ui/multiplayer/sc_ui.js
@@ -1,4 +1,4 @@
-import { notify, notifyError } from "../elements/notification";
+import { notify } from "../elements/notification";
 
 export default class ScUi extends CABLES.EventTarget
 {
@@ -6,40 +6,12 @@ export default class ScUi extends CABLES.EventTarget
     {
         super();
         this._connection = connection;
-        this.notifyPingTimeout = true;
-        this.notifyNetworkError = true;
 
         this._connection.on("onInfoMessage", (payload) =>
         {
             if (payload.name === "notify")
             {
                 notify(payload.title, payload.text);
-            }
-        });
-
-        this._connection.on("connectionError", (payload) =>
-        {
-            if (this.notifyNetworkError)
-            {
-                notifyError("network error", payload.message);
-                this.notifyNetworkError = false;
-            }
-        });
-
-        this._connection.on("onPingAnswer", (msg) =>
-        {
-            if (msg.clientId === this._connection.clientId)
-            {
-                this.notifyPingTimeout = true;
-            }
-        });
-
-        this._connection.on("onPingTimeout", (payload) =>
-        {
-            if (this.notifyPingTimeout)
-            {
-                this.notifyPingTimeout = false;
-                notify("network warning", "received last ping more than " + payload.seconds + " seconds ago");
             }
         });
     }
