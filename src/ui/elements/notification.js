91bb0cc54de87f2bbcf1c3a62d81c5e3eaef2b71
Stephan Maihöfer <stephan@undev.de>
2022-03-14 18:27:22 +0100

    add configuration options to notifications (timeout, close, force)

--- src/ui/elements/notification.js
+++ src/ui/elements/notification.js
@@ -2,8 +2,47 @@
 let lastNotify = "";
 let lastText = "";
 
-export function notifyError(title, text)
+/**
+ * configuration object for loading a patch
+ * @typedef {Object} NotificationDisplayOptions
+ * @hideconstructor
+ * @property {Number|Boolean} [timeout=2000] fade out notification after x ms, `false` to disable
+ * @property {Boolean} [closeable=false] show closing button on notification
+ * @property {Boolean} [force=false] force showing of notification even if last one was the same
+ */
+
+/**
+ * notifyError displays an error as a toast-notification
+ *
+ *
+ * @param title
+ * @param text
+ * @param {NotificationDisplayOptions} options The option object.
+ * @class
+ * @example
+ * new notifyError("error", "something broke",
+ * {
+ *     "timeout": false,
+ *     "closeable": true,
+ * });
+ */
+export function notifyError(title, text, options = {})
 {
+    const timeout = options.hasOwnProperty("timeout") ? options.timeout : 2000;
+    const closeable = options.closeable || false;
+    const force = options.hasOwnProperty("force") ? options.force : true;
+
+    if (!force)
+    {
+        if (title === lastNotify && text === lastText)
+        {
+            return;
+        }
+    }
+
+    lastNotify = title;
+    lastText = text;
+
     iziToast.error(
         {
             "position": "topRight", // bottomRight, bottomLeft, topRight, topLeft, topCenter, bottomCenter, center
@@ -12,23 +51,45 @@ export function notifyError(title, text)
             "message": text || "",
             "progressBar": false,
             "animateInside": false,
-            "close": false,
-            "timeout": 2000
+            "close": closeable,
+            "timeout": timeout
         });
 }
 
-export function notify(title, text)
+/**
+ * notify displays a toast-notification
+ *
+ *
+ * @param title
+ * @param text
+ * @param {NotificationDisplayOptions} options The option object.
+ * @class
+ * @example
+ * new notifyError("update", "cables has been updated",
+ * {
+ *     "timeout": 1000,
+ *     "closeable": false
+ * });
+ */
+export function notify(title, text, options = {})
 {
     if (gui.isRemoteClient) return;
 
-    if (title == lastNotify && text == lastText)
+    const timeout = options.timeout || 2000;
+    const closeable = options.closeable || false;
+    const force = options.hasOwnProperty("force") ? options.force : false;
+
+    if (!force)
     {
-        setTimeout(function ()
+        if (title == lastNotify && text == lastText)
         {
-            lastNotify = "";
-            lastText = "";
-        }, 1000);
-        return;
+            setTimeout(function ()
+            {
+                lastNotify = "";
+                lastText = "";
+            }, 1000);
+            return;
+        }
     }
 
     lastNotify = title;
@@ -42,7 +103,7 @@ export function notify(title, text)
             "message": text || "",
             "progressBar": false,
             "animateInside": false,
-            "close": false,
-            "timeout": 2000
+            "close": closeable,
+            "timeout": timeout
         });
 }
