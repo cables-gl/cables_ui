f8ca39e0c0a96509d474393c46006cf6e24ed607
Stephan Maihöfer <stephan@undev.de>
2022-03-31 18:49:41 +0200

    iframe size

--- src/ui/components/patchview.js
+++ src/ui/components/patchview.js
@@ -508,7 +508,7 @@ export default class PatchView extends CABLES.EventTarget
         let html = "<div class=\"panel\">";
         this.checkPatchErrors();
 
-        if (ele.byId("patch_defaultpanel")) return;
+        if (ele.byId("patchsummary")) return;
 
         const project = gui.project();
         if (!gui.user.isPatchOwner && !project.users.includes(gui.user.id))
