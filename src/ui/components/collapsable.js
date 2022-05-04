87822123e930c4b7e72763c0d60770a3a13f28f5
pandrr <tom@undev.de>
2022-05-04 14:13:18 +0200

    collapsable

---
+++ src/ui/components/collapsable.js
@@ -0,0 +1,54 @@
+import ele from "../utils/ele";
+
+const Collapsable = {};
+
+export default Collapsable;
+
+Collapsable.setup = function (parentEle, childEle, collapsed)
+{
+    if (!childEle || !parentEle) return;
+
+    parentEle.classList.add("collapsable");
+
+    if (parentEle.innerHTML.indexOf("icon-chevron") == -1)
+        parentEle.innerHTML = "<span class=\"icon icon-chevron-down\"></span>" + parentEle.innerHTML;
+
+    Collapsable._setGroupCollapsed(parentEle, childEle, collapsed);
+
+    if (!collapsed) Collapsable._toggleGroupElements(parentEle, childEle);
+
+    parentEle.addEventListener("click", () =>
+    {
+        console.log("click....");
+
+        Collapsable._toggleGroupElements(parentEle, childEle);
+    });
+};
+
+Collapsable._setGroupCollapsed = (parentEle, childEle, collapsed) =>
+{
+    if (!collapsed)
+    {
+        childEle.classList.remove("hidden");
+        parentEle.children[0].classList.add("icon-chevron-down");
+        parentEle.children[0].classList.remove("icon-chevron-right");
+    }
+    else
+    {
+        childEle.classList.add("hidden");
+        parentEle.children[0].classList.remove("icon-chevron-down");
+        parentEle.children[0].classList.add("icon-chevron-right");
+    }
+};
+
+Collapsable._toggleGroupElements = (parentEle, childEle) =>
+{
+    // const els = ele.byClassAll(name);
+    // for (let i = 0; i < els.length; i++)
+    // {
+    childEle.classList.toggle("hidden");
+    // }
+
+    parentEle.children[0].classList.toggle("icon-chevron-down");
+    parentEle.children[0].classList.toggle("icon-chevron-right");
+};
