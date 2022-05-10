7df7ec39597e1011eaefc713cc007c7fee1c6e86
Stephan Maihöfer <stephan@undev.de>
2022-05-10 15:00:18 +0200

    less pings

--- src/ui/multiplayer/sc_connection.js
+++ src/ui/multiplayer/sc_connection.js
@@ -12,8 +12,8 @@ export default class ScConnection extends CABLES.EventTarget
     {
         super();
 
-        this.PING_INTERVAL = 5000;
-        this.PING_ANSWER_INTERVAL = 2000;
+        this.PING_INTERVAL = 10000;
+        this.PING_ANSWER_INTERVAL = 5000;
         this.PINGS_TO_TIMEOUT = 2;
         this.OWN_PINGS_TO_TIMEOUT = 5;
 
