55d5fa7fb5a1ccd04c584df01c6c31d43195cbd4
Stephan Maihöfer <stephan@undev.de>
2022-05-24 18:10:17 +0200

    ping timeout

--- src/ui/multiplayer/sc_connection.js
+++ src/ui/multiplayer/sc_connection.js
@@ -12,7 +12,7 @@ export default class ScConnection extends CABLES.EventTarget
     {
         super();
 
-        this.PING_INTERVAL = 5000;
+        this.PING_INTERVAL = 2500;
         this.PING_ANSWER_INTERVAL = 2500;
         this.PINGS_TO_TIMEOUT = 5;
         this.OWN_PINGS_TO_TIMEOUT = 5;
