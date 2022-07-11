797a40f577afa9d9e81476f58b1ac0cce8360902
Stephan Maihöfer <stephan@undev.de>
2022-07-11 15:51:19 +0200

    add markdown download for keyboard shortcuts admin/staff - pandrr/cables/issues/3728

--- src/ui/utils/handlebars.js
+++ src/ui/utils/handlebars.js
@@ -85,4 +85,13 @@ export function initHandleBarsHelper()
             return options.inverse(this);
         }
     });
+
+    Handlebars.registerHelper("toUpperCase", function (str)
+    {
+        if (str && typeof str === "string")
+        {
+            return str.charAt(0).toUpperCase() + str.slice(1);
+        }
+        return "";
+    });
 }
