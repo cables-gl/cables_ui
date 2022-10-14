ac5cc53a59526f0c35aff8c48588f7aaea9fd45d
pandrr <tom@undev.de>
2022-10-14 09:48:27 +0200

    check

--- src/ui/defaultops.js
+++ src/ui/defaultops.js
@@ -53,7 +53,7 @@ export default
     {
         if (p && p.direction == CONSTANTS.PORT.PORT_DIR_OUT)
         {
-            if (p.type == CONSTANTS.OP.OP_PORT_TYPE_STRING) return ["Ops.Ui.VizString"];
+            if (p.type == CONSTANTS.OP.OP_PORT_TYPE_STRING) return ["Ops.Ui.VizString", "Ops.Ui.VizStringLong"];
             else if (p.type == CONSTANTS.OP.OP_PORT_TYPE_VALUE && p.uiAttribs.display == "bool") return ["Ops.Ui.VizBool", "Ops.Ui.VizNumber"];
             else if (p.type == CONSTANTS.OP.OP_PORT_TYPE_VALUE) return ["Ops.Ui.VizNumber", "Ops.Ui.VizGraph", "Ops.Ui.VizNumberBar"];
             else if (p.type == CONSTANTS.OP.OP_PORT_TYPE_ARRAY) return ["Ops.Ui.VizArrayTable"];
