cee12f9efe8337d268c95dcbc8c8724055a9ffc0
pandrr <tom@tomkom.net>
2023-08-18 13:51:46 +0200

    fix x disconnects

--- src/ui/op_core_extend.js
+++ src/ui/op_core_extend.js
@@ -75,7 +75,7 @@ export default function extendCore()
 
     CABLES.Op.prototype.unLinkReconnectOthers = function ()
     {
-        this.unLinkOptions(true, false);
+        this.unLinkOptions(false, false);
     };
 
     CABLES.Op.prototype.unLinkTemporary = function ()
