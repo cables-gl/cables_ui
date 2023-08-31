77086ce4441bbf6e62425af69c2e128db3bd8eb7
Stephan Maihöfer <stephan@undev.de>
2023-08-31 13:38:18 +0200

    change current project name when updating - pandrr/cables/issues/5038

--- src/ui/gui.js
+++ src/ui/gui.js
@@ -996,6 +996,7 @@ export default class Gui
             ele.byId("patchname").innerHTML = name;
             ele.byId("patchname").dataset.patchname = name;
             gui.corePatch().name = name;
+            if (this._currentProject) this._currentProject.name = name;
         }
     }
 
