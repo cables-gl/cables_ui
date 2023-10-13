09da879d78dfd44e0cd44df96765222af71e6863
pandrr <tom@tomkom.net>
2023-10-13 18:50:06 +0200

    canvasmanager

--- src/ui/components/canvas/canvasmanager.js
+++ src/ui/components/canvas/canvasmanager.js
@@ -4,6 +4,12 @@ export default class CanvasManager
     {
         this._curContextIdx = 0;
         this._contexts = [];
+        this.subWindow = null;
+
+        window.addEventListener("beforeunload", () =>
+        {
+            if (this.subWindow) this.subWindow.close();
+        });
     }
 
     currentCanvas()
@@ -111,18 +117,18 @@ export default class CanvasManager
         let id = CABLES.uuid();
         this.subWindow = window.open("", "view#" + id, "width=" + 500 + ",height=" + 500 + ",directories=0,titlebar=0,toolbar=0,location=0,status=0,menubar=0,scrollbars=no,resizable=yes,popup=true");
         if (!this.subWindow) return;
-        let document = this.subWindow.document;
-        document.title = "jo";
+        let nDocument = this.subWindow.document;
+        nDocument.title = "jo";
 
-        let body = document.body;
+        let nBody = nDocument.body;
 
-        // const img = document.createElement("img");
+        // const img = nDocument.createElement("img");
         // img.setAttribute("src", "http://localhost:5711/assets/6528ff86a3da3169c9cfef49/production_0_side.jpg?rnd629ea44b-fff0-4f1d-885c-db95d63aae30/production_0_side.jpg?rnd=629ea44b-fff0-4f1d-885c-db95d63aae30");
         // img.onload = () => { console.log("load"); };
         // body.appendChild(img);
         // console.log("huhu");
 
-        const style = document.createElement("link");
+        const style = nDocument.createElement("link");
         style.setAttribute("href", "/ui/css/style-dark.css");
         style.setAttribute("rel", "stylesheet");
         style.setAttribute("type", "text/css");
@@ -130,24 +136,21 @@ export default class CanvasManager
 
         style.onload = () => { console.log("load"); };
 
-        document.head.appendChild(style);
+        nDocument.head.appendChild(style);
 
         // <link rel="stylesheet" type="text/css" media="all" href="css/style-dark.css">
 
-        body.classList.add("cablesCssUi");
+        nBody.classList.add("cablesCssUi");
 
-        const containerEle = document.createElement("div");
+        const containerEle = nDocument.createElement("div");
 
         containerEle.classList.add("bgpatternDark");
         containerEle.classList.add("bgPatternDark");
         containerEle.style.width = "100%";
         containerEle.style.height = "100%";
-        body.appendChild(containerEle);
+        nBody.appendChild(containerEle);
 
         containerEle.id = "cablescanvas";
-
-        // overflow: hidden; width: 891px; height: 552px; position: absolute; right: 0px; z-index: 10; left: initial; transform-origin: right top; transform: scale(1);
-
 
         const p = gui.corePatch().cgl.canvas.parentElement;
 
@@ -155,6 +158,12 @@ export default class CanvasManager
         {
             containerEle.appendChild(p.childNodes[0]);
         }
+
+        // const cablesEles = document.body.getElementsByClassName("cablesEle");
+        // for (let i = 0; i < cablesEles.length; i++)nBody.appendChild(cablesEles[i]);
+
+        while (document.body.getElementsByClassName("cablesEle").length > 0)
+            nBody.appendChild(document.body.getElementsByClassName("cablesEle")[0]);
 
         this.subWindow.addEventListener("resize", () =>
         {
@@ -169,6 +178,10 @@ export default class CanvasManager
             {
                 p.appendChild(containerEle.childNodes[0]);
             }
+
+            while (nBody.getElementsByClassName("cablesEle").length > 0)
+                document.body.appendChild(nBody.getElementsByClassName("cablesEle")[0]);
+            // for (let i = 0; i < ncablesEles.length; i++)
 
             gui.corePatch().cgl.updateSize();
             this.poppedOut = false;
