9248ccf4f10f3649c1034c076a4d2e75f8331df6
pandrr <tom@tomkom.net>
2023-10-19 18:01:10 +0200

    fix error

--- src/ui/index.js
+++ src/ui/index.js
@@ -54,6 +54,7 @@ import { arrayContains, uniqueArray } from "./utils/helper";
 import LogFilter from "./utils/loggerfilter";
 import undo from "./utils/undo";
 import TabPortObjectInspect from "./components/tabs/tab_portobjectionspect";
+import MetaCode from "./components/tabs/meta_code";
 
 CABLES = CABLES || {};
 CABLES.UI = CABLES.UI || {};
