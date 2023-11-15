bb908ec305af14dee3277a553486e6abd2b8bb9e
pandrr <tom@tomkom.net>
2023-11-15 13:09:49 +0100

    check defaultopnames on startup

--- src/ui/defaultops.js
+++ src/ui/defaultops.js
@@ -5,102 +5,145 @@
 
 import { CONSTANTS } from "../../../cables/src/core/constants";
 
+
+export const defaultOpNames =
+{
+    "number": "Ops.Value.Number",
+    "defaultOpImage": "Ops.Gl.Texture_v2",
+    "defaultOpAudio": "Ops.WebAudio.AudioBuffer_v2",
+    "defaultOpVideo": "Ops.Gl.Textures.VideoTexture_v3",
+    "defaultOpGltf": "Ops.Gl.GLTF.GltfScene_v4",
+    "defaultOpJson": "Ops.Json.HttpRequest_v3",
+    "defaultOpExr": "Ops.Gl.Textures.ExrTexture",
+    "VarSetNumber": "Ops.Vars.VarSetNumber_v2",
+    "VarTriggerNumber": "Ops.Vars.VarTriggerNumber",
+    "VarGetNumber": "Ops.Vars.VarGetNumber_v2",
+    "VarSetObject": "Ops.Vars.VarSetObject_v2",
+    "VarTriggerObject": "Ops.Vars.VarTriggerObject",
+    "VarGetObject": "Ops.Vars.VarGetObject_v2",
+    "VarSetTexture": "Ops.Vars.VarSetTexture_v2",
+    "VarGetTexture": "Ops.Vars.VarGetTexture_v2",
+    "VarSetArray": "Ops.Vars.VarSetArray_v2",
+    "VarTriggerArray": "Ops.Vars.VarTriggerArray",
+    "VarGetArray": "Ops.Vars.VarGetArray_v2",
+    "VarSetString": "Ops.Vars.VarSetString_v2",
+    "VarTriggerString": "Ops.Vars.VarTriggerString",
+    "VarGetString": "Ops.Vars.VarGetString",
+    "VarSetTrigger": "Ops.Trigger.TriggerSend",
+    "VarGetTrigger": "Ops.Trigger.TriggerReceive",
+    "defaultFont": "Ops.Html.FontFile_v2",
+
+    "blueprint": "Ops.Dev.Blueprint",
+    "blueprintTemplate": "Ops.Templates.Blueprint2",
+
+    "subPatch": "Ops.Ui.SubPatch",
+    "subPatch2": "Ops.Dev.SubpatchNew",
+    "subPatchInput2": "Ops.Dev.Ui.PatchInput",
+    "subPatchOutput2": "Ops.Dev.Ui.PatchOutput",
+
+    "uiArea": "Ops.Ui.Area",
+    "defaultOpSvg": "Ops.Gl.Textures.TextureSVG_v2",
+    "defaultOpVizTexture": "Ops.Ui.VizTexture",
+
+    "NumberToString": "Ops.String.NumberToString_v2",
+    "TriggerOnChangeNumber": "Ops.Value.TriggerOnChangeNumber",
+    "TriggerOnChangeString": "Ops.Trigger.TriggerOnChangeString",
+    "TriggerCounter": "Ops.Trigger.TriggerCounter",
+    "parseFloat": "Ops.String.ParseFloat",
+    "arrayLength": "Ops.Array.ArrayLength_v2",
+    "StringToArray": "Ops.Array.StringToArray_v2",
+    "arrayToString": "Ops.Array.ArrayToString_v3",
+    "stringConcat": "Ops.String.Concat_v2",
+
+    "VizArrayTable": "Ops.Ui.VizArrayTable",
+    "VizBool": "Ops.Ui.VizBool",
+    "VizGraph": "Ops.Ui.VizGraph",
+    "VizNumber": "Ops.Ui.VizNumber",
+    "VizNumberBar": "Ops.Ui.VizNumberBar",
+    "VizObject": "Ops.Ui.VizObject",
+    "VizString": "Ops.Ui.VizString",
+    "VizStringLong": "Ops.Ui.VizStringLong",
+    "VizTexture": "Ops.Ui.VizTexture",
+    "VizTextureTable": "Ops.Ui.VizTextureTable",
+
+    "string": "Ops.String.String_v2",
+    "stringEditor": "Ops.String.StringEditor",
+    "sequence": "Ops.Trigger.Sequence",
+    "divElement": "Ops.Html.DivElement_v3",
+    "customShader": "Ops.Gl.Shader.CustomShader_v2",
+
+    "GreaterThan": "Ops.Math.Compare.GreaterThan",
+    "LessThan": "Ops.Math.Compare.LessThan",
+    "Sum": "Ops.Math.Sum",
+    "Subtract": "Ops.Math.Subtract",
+    "Divide": "Ops.Math.Divide",
+    "Multiply": "Ops.Math.Multiply",
+    "Equals": "Ops.Math.Compare.Equals",
+    "ArraySum": "Ops.Array.ArraySum",
+    "ArraySubtract": "Ops.Array.ArraySubtract",
+    "ArrayDivide": "Ops.Array.ArrayDivide",
+    "ArrayMultiply": "Ops.Array.ArrayMultiply",
+
+};
+
 const defaultOps = {
-    "defaultOpNames":
-    {
-        "number": "Ops.Value.Number",
-        "defaultOpImage": "Ops.Gl.Texture_v2",
-        "defaultOpAudio": "Ops.WebAudio.AudioBuffer_v2",
-        "defaultOpVideo": "Ops.Gl.Textures.VideoTexture_v3",
-        "defaultOpGltf": "Ops.Gl.GLTF.GltfScene_v4",
-        "defaultOpJson": "Ops.Json.HttpRequest_v3",
-        "defaultOpExr": "Ops.Gl.Textures.ExrTexture",
-        "VarSetNumber": "Ops.Vars.VarSetNumber_v2",
-        "VarTriggerNumber": "Ops.Vars.VarTriggerNumber",
-        "VarGetNumber": "Ops.Vars.VarGetNumber_v2",
-        "VarSetObject": "Ops.Vars.VarSetObject_v2",
-        "VarTriggerObject": "Ops.Vars.VarTriggerObject",
-        "VarGetObject": "Ops.Vars.VarGetObject_v2",
-        "VarSetTexture": "Ops.Vars.VarSetTexture_v2",
-        "VarGetTexture": "Ops.Vars.VarGetTexture_v2",
-        "VarSetArray": "Ops.Vars.VarSetArray_v2",
-        "VarTriggerArray": "Ops.Vars.VarTriggerArray",
-        "VarGetArray": "Ops.Vars.VarGetArray_v2",
-        "VarSetString": "Ops.Vars.VarSetString_v2",
-        "VarTriggerString": "Ops.Vars.VarTriggerString",
-        "VarGetString": "Ops.Vars.VarGetString",
-        "VarSetTrigger": "Ops.Trigger.TriggerSend",
-        "VarGetTrigger": "Ops.Trigger.TriggerReceive",
-        "defaultFont": "Ops.Html.FontFile_v2",
-
-        "blueprint": "Ops.Dev.Blueprint",
-        "blueprintTemplate": "Ops.Templates.Blueprint2",
-
-        "subPatch": "Ops.Ui.SubPatch",
-        "subPatch2": "Ops.Dev.SubpatchNew",
-        "subPatchInput2": "Ops.Dev.Ui.PatchInput",
-        "subPatchOutput2": "Ops.Dev.Ui.PatchOutput",
-
-        "uiArea": "Ops.Ui.Area",
-        "defaultOpSvg": "Ops.Gl.Textures.TextureSVG_v2",
-        "defaultOpVizTexture": "Ops.Ui.VizTexture",
-
-    },
+    "defaultOpNames": defaultOpNames,
     "converterOps":
     [
         {
             "typeFrom": CONSTANTS.OP.OP_PORT_TYPE_VALUE,
             "typeTo": CONSTANTS.OP.OP_PORT_TYPE_STRING,
-            "op": "Ops.String.NumberToString_v2",
+            "op": defaultOpNames.NumberToString,
             "portIn": "Number",
             "portOut": "Result",
         },
         {
             "typeFrom": CONSTANTS.OP.OP_PORT_TYPE_VALUE,
             "typeTo": CONSTANTS.OP.OP_PORT_TYPE_TRIGGER,
-            "op": "Ops.Value.TriggerOnChangeNumber",
+            "op": defaultOpNames.TriggerOnChangeNumber,
             "portIn": "Value",
             "portOut": "Next",
         },
         {
             "typeFrom": CONSTANTS.OP.OP_PORT_TYPE_STRING,
             "typeTo": CONSTANTS.OP.OP_PORT_TYPE_TRIGGER,
-            "op": "Ops.Trigger.TriggerOnChangeString",
+            "op": defaultOpNames.TriggerOnChangeString,
             "portIn": "String",
             "portOut": "Changed",
         },
         {
             "typeFrom": CONSTANTS.OP.OP_PORT_TYPE_TRIGGER,
             "typeTo": CONSTANTS.OP.OP_PORT_TYPE_VALUE,
-            "op": "Ops.Trigger.TriggerCounter",
+            "op": defaultOpNames.triggerCounter,
             "portIn": "exe",
             "portOut": "timesTriggered",
         },
         {
             "typeFrom": CONSTANTS.OP.OP_PORT_TYPE_STRING,
             "typeTo": CONSTANTS.OP.OP_PORT_TYPE_VALUE,
-            "op": "Ops.String.ParseFloat",
+            "op": defaultOpNames.parseFloat,
             "portIn": "String",
             "portOut": "Number",
         },
         {
             "typeFrom": CONSTANTS.OP.OP_PORT_TYPE_ARRAY,
             "typeTo": CONSTANTS.OP.OP_PORT_TYPE_VALUE,
-            "op": "Ops.Array.ArrayLength_v2",
+            "op": defaultOpNames.arrayLength,
             "portIn": "array",
             "portOut": "length",
         },
         {
             "typeFrom": CONSTANTS.OP.OP_PORT_TYPE_STRING,
             "typeTo": CONSTANTS.OP.OP_PORT_TYPE_ARRAY,
-            "op": "Ops.Array.StringToArray_v2",
+            "op": defaultOpNames.StringToArray,
+
             "portIn": "text",
             "portOut": "array",
         },
         {
             "typeFrom": CONSTANTS.OP.OP_PORT_TYPE_ARRAY,
             "typeTo": CONSTANTS.OP.OP_PORT_TYPE_STRING,
-            "op": "Ops.Array.ArrayToString_v3",
+            "op": defaultOpNames.arrayToString,
             "portIn": "Array",
             "portOut": "Result",
         },
@@ -111,52 +154,52 @@ const defaultOps = {
     {
         "default":
         {
-            ">": "Ops.Math.Compare.GreaterThan",
-            "<": "Ops.Math.Compare.LessThan",
-            "+": "Ops.Math.Sum",
-            "-": "Ops.Math.Subtract",
-            "/": "Ops.Math.Divide",
-            "*": "Ops.Math.Multiply",
-            "=": "Ops.Math.Equals",
+            "GreaterThan": defaultOpNames.GreaterThan,
+            "LessThan": defaultOpNames.LessThan,
+            "Sum": defaultOpNames.Sum,
+            "Subtract": defaultOpNames.Subtract,
+            "Divide": defaultOpNames.Divide,
+            "Multiply": defaultOpNames.Multiply,
+            "Equals": defaultOpNames.Equals,
         },
         "array":
         {
-            "+": "Ops.Array.ArraySum",
-            "-": "Ops.Array.ArraySubtract",
-            "/": "Ops.Array.ArrayDivide",
-            "*": "Ops.Array.ArrayMultiply",
+            "ArraySum": defaultOpNames.ArraySum,
+            "ArraySubtract": defaultOpNames.ArraySubtract,
+            "ArrayDivide": defaultOpNames.ArrayDivide,
+            "ArrayMultiply": defaultOpNames.ArrayMultiply,
         },
         "string":
         {
-            "+": "Ops.String.Concat_v2"
+            "+": defaultOpNames.stringConcat
         }
     },
     "getVizOpsForPortLink": (p, l) =>
     {
         if (p && p.direction == CONSTANTS.PORT.PORT_DIR_OUT)
         {
-            if (p.type == CONSTANTS.OP.OP_PORT_TYPE_STRING) return ["Ops.Ui.VizString", "Ops.Ui.VizStringLong"];
-            else if (p.type == CONSTANTS.OP.OP_PORT_TYPE_VALUE && (p.uiAttribs.display == "bool" || p.uiAttribs.display == "boolnum")) return ["Ops.Ui.VizBool", "Ops.Ui.VizNumber"];
-            else if (p.type == CONSTANTS.OP.OP_PORT_TYPE_VALUE) return ["Ops.Ui.VizNumber", "Ops.Ui.VizGraph", "Ops.Ui.VizNumberBar"];
-            else if (p.type == CONSTANTS.OP.OP_PORT_TYPE_ARRAY) return ["Ops.Ui.VizArrayTable"];
-            else if (p.type == CONSTANTS.OP.OP_PORT_TYPE_OBJECT && p.uiAttribs.objType == "texture") return ["Ops.Ui.VizTexture", "Ops.Ui.VizTextureTable", "Ops.Ui.VizObject"];
-            else if (p.type == CONSTANTS.OP.OP_PORT_TYPE_OBJECT) return ["Ops.Ui.VizObject"];
+            if (p.type == CONSTANTS.OP.OP_PORT_TYPE_STRING) return [defaultOpNames.VizString, defaultOpNames.VizStringLong];
+            else if (p.type == CONSTANTS.OP.OP_PORT_TYPE_VALUE && (p.uiAttribs.display == "bool" || p.uiAttribs.display == "boolnum")) return [defaultOpNames.VizBool, defaultOpNames.VizNumber];
+            else if (p.type == CONSTANTS.OP.OP_PORT_TYPE_VALUE) return [defaultOpNames.VizNumber, defaultOpNames.VizGraph, defaultOpNames.VizNumberBar];
+            else if (p.type == CONSTANTS.OP.OP_PORT_TYPE_ARRAY) return [defaultOpNames.VizArrayTable];
+            else if (p.type == CONSTANTS.OP.OP_PORT_TYPE_OBJECT && p.uiAttribs.objType == "texture") return [defaultOpNames.VizTexture, defaultOpNames.VizTextureTable, defaultOpNames.VizObject];
+            else if (p.type == CONSTANTS.OP.OP_PORT_TYPE_OBJECT) return [defaultOpNames.VizObject];
         }
     },
     "getOpsForPortLink": (p, l) =>
     {
         if (p && p.direction == CONSTANTS.PORT.PORT_DIR_IN)
         {
-            if (p.type == CONSTANTS.OP.OP_PORT_TYPE_STRING) return ["Ops.String.String_v2", "Ops.String.StringEditor"];
-            else if (p.type == CONSTANTS.OP.OP_PORT_TYPE_VALUE) return ["Ops.Value.Number"];
-            else if (p.type == CONSTANTS.OP.OP_PORT_TYPE_FUNCTION) return ["Ops.Trigger.Sequence"];
-            else if (p.type == CONSTANTS.OP.OP_PORT_TYPE_OBJECT && p.uiAttribs.objType == "texture") return [CABLES.UI.DEFAULTOPNAMES.defaultOpImage];
-            else if (p.type == CONSTANTS.OP.OP_PORT_TYPE_OBJECT && p.uiAttribs.objType == "element") return ["Ops.Html.DivElement_v2"];
-            else if (p.type == CONSTANTS.OP.OP_PORT_TYPE_OBJECT && p.uiAttribs.objType == "shader") return ["Ops.Gl.Shader.CustomShader_v2"];
+            if (p.type == CONSTANTS.OP.OP_PORT_TYPE_STRING) return [defaultOpNames.string, defaultOpNames.StringEditor];
+            else if (p.type == CONSTANTS.OP.OP_PORT_TYPE_VALUE) return [defaultOpNames.Number];
+            else if (p.type == CONSTANTS.OP.OP_PORT_TYPE_FUNCTION) return [defaultOpNames.sequence];
+            else if (p.type == CONSTANTS.OP.OP_PORT_TYPE_OBJECT && p.uiAttribs.objType == "texture") return [defaultOpNames.defaultOpImage];
+            else if (p.type == CONSTANTS.OP.OP_PORT_TYPE_OBJECT && p.uiAttribs.objType == "element") return [defaultOpNames.divElement];
+            else if (p.type == CONSTANTS.OP.OP_PORT_TYPE_OBJECT && p.uiAttribs.objType == "shader") return [defaultOpNames.customShader];
         }
         if (p && p.direction == CONSTANTS.PORT.PORT_DIR_OUT)
         {
-            if (p.type == CONSTANTS.OP.OP_PORT_TYPE_FUNCTION) return ["Ops.Trigger.Sequence"];
+            if (p.type == CONSTANTS.OP.OP_PORT_TYPE_FUNCTION) return [defaultOpNames.sequence];
         }
     },
     "getOpsForFilename": (filename) =>
@@ -165,14 +208,14 @@ const defaultOps = {
 
         filename = filename.toLowerCase();
 
-        if (filename.endsWith(".png") || filename.endsWith(".jpg") || filename.endsWith(".jpeg") || filename.endsWith(".jxl") || filename.endsWith(".webp")) ops.push(CABLES.UI.DEFAULTOPNAMES.defaultOpImage);
-        else if (filename.endsWith(".ogg") || filename.endsWith(".wav") || filename.endsWith(".mp3") || filename.endsWith(".m4a") || filename.endsWith(".aac")) ops.push(CABLES.UI.DEFAULTOPNAMES.defaultOpAudio);
-        else if (filename.endsWith(".mp4") || filename.endsWith(".m4a") || filename.endsWith(".mpg") || filename.endsWith(".webm") || filename.endsWith(".mkv") || filename.endsWith(".mov")) ops.push(CABLES.UI.DEFAULTOPNAMES.defaultOpVideo);
-        else if (filename.endsWith(".glb")) ops.push(CABLES.UI.DEFAULTOPNAMES.defaultOpGltf);
-        else if (filename.endsWith(".json")) ops.push(CABLES.UI.DEFAULTOPNAMES.defaultOpJson);
-        else if (filename.endsWith(".ttf") || filename.endsWith(".woff") || filename.endsWith(".woff2") || filename.endsWith(".otf")) ops.push(CABLES.UI.DEFAULTOPNAMES.defaultFont);
-        else if (filename.endsWith(".exr")) ops.push(CABLES.UI.DEFAULTOPNAMES.defaultOpExr);
-        else if (filename.endsWith(".svg")) ops.push(CABLES.UI.DEFAULTOPNAMES.defaultOpSvg);
+        if (filename.endsWith(".png") || filename.endsWith(".jpg") || filename.endsWith(".jpeg") || filename.endsWith(".jxl") || filename.endsWith(".webp")) ops.push(defaultOpNames.defaultOpImage);
+        else if (filename.endsWith(".ogg") || filename.endsWith(".wav") || filename.endsWith(".mp3") || filename.endsWith(".m4a") || filename.endsWith(".aac")) ops.push(defaultOpNames.defaultOpAudio);
+        else if (filename.endsWith(".mp4") || filename.endsWith(".m4a") || filename.endsWith(".mpg") || filename.endsWith(".webm") || filename.endsWith(".mkv") || filename.endsWith(".mov")) ops.push(defaultOpNames.defaultOpVideo);
+        else if (filename.endsWith(".glb")) ops.push(defaultOpNames.defaultOpGltf);
+        else if (filename.endsWith(".json")) ops.push(defaultOpNames.defaultOpJson);
+        else if (filename.endsWith(".ttf") || filename.endsWith(".woff") || filename.endsWith(".woff2") || filename.endsWith(".otf")) ops.push(defaultOpNames.defaultFont);
+        else if (filename.endsWith(".exr")) ops.push(defaultOpNames.defaultOpExr);
+        else if (filename.endsWith(".svg")) ops.push(defaultOpNames.defaultOpSvg);
         return ops;
     },
     "getVarGetterOpNameByType": (type, port) =>
@@ -185,41 +228,41 @@ const defaultOps = {
 
         if (type == CABLES.OP_PORT_TYPE_VALUE)
         {
-            opSetterName = CABLES.UI.DEFAULTOPNAMES.VarSetNumber;
-            opGetterName = CABLES.UI.DEFAULTOPNAMES.VarGetNumber;
-            opSetTriggerName = CABLES.UI.DEFAULTOPNAMES.VarTriggerNumber;
+            opSetterName = defaultOpNames.VarSetNumber;
+            opGetterName = defaultOpNames.VarGetNumber;
+            opSetTriggerName = defaultOpNames.VarTriggerNumber;
         }
         else if (type == CABLES.OP_PORT_TYPE_OBJECT)
         {
-            opSetterName = CABLES.UI.DEFAULTOPNAMES.VarSetObject;
-            opGetterName = CABLES.UI.DEFAULTOPNAMES.VarGetObject;
-            opSetTriggerName = CABLES.UI.DEFAULTOPNAMES.VarTriggerObject;
+            opSetterName = defaultOpNames.VarSetObject;
+            opGetterName = defaultOpNames.VarGetObject;
+            opSetTriggerName = defaultOpNames.VarTriggerObject;
 
             if (port && port.uiAttribs.objType == "texture")
             {
-                opSetterName = CABLES.UI.DEFAULTOPNAMES.VarSetTexture;
-                opGetterName = CABLES.UI.DEFAULTOPNAMES.VarGetTexture;
+                opSetterName = defaultOpNames.VarSetTexture;
+                opGetterName = defaultOpNames.VarGetTexture;
             }
         }
         else if (type == CABLES.OP_PORT_TYPE_ARRAY)
         {
-            opSetterName = CABLES.UI.DEFAULTOPNAMES.VarSetArray;
-            opGetterName = CABLES.UI.DEFAULTOPNAMES.VarGetArray;
-            opSetTriggerName = CABLES.UI.DEFAULTOPNAMES.VarTriggerArray;
+            opSetterName = defaultOpNames.VarSetArray;
+            opGetterName = defaultOpNames.VarGetArray;
+            opSetTriggerName = defaultOpNames.VarTriggerArray;
         }
         else if (type == CABLES.OP_PORT_TYPE_STRING)
         {
-            opSetterName = CABLES.UI.DEFAULTOPNAMES.VarSetString;
-            opGetterName = CABLES.UI.DEFAULTOPNAMES.VarGetString;
-            opSetTriggerName = CABLES.UI.DEFAULTOPNAMES.VarTriggerString;
+            opSetterName = defaultOpNames.VarSetString;
+            opGetterName = defaultOpNames.VarGetString;
+            opSetTriggerName = defaultOpNames.VarTriggerString;
         }
         else if (type == CABLES.OP_PORT_TYPE_FUNCTION)
         {
             portName = "Trigger";
             portNameOut = "Triggered";
 
-            opSetterName = CABLES.UI.DEFAULTOPNAMES.VarSetTrigger;
-            opGetterName = CABLES.UI.DEFAULTOPNAMES.VarGetTrigger;
+            opSetterName = defaultOpNames.VarSetTrigger;
+            opGetterName = defaultOpNames.VarGetTrigger;
             opSetTriggerName = opSetterName;
         }
         else
