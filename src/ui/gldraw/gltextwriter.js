49fcd2faff6f1f6a130c4eed99f42a514808e0bd
pandrr <tom@tomkom.net>
2023-11-15 11:58:38 +0100

    bookmark icon positioning

--- src/ui/gldraw/gltextwriter.js
+++ src/ui/gldraw/gltextwriter.js
@@ -11,9 +11,7 @@ export default class GlTextWriter
         if (!cgl) throw new Error("[gltextwriter] no cgl");
 
         this._rectDrawer = new GlRectInstancer(cgl, { "initNum": options.initNum, "name": "textrects_" + this._name });
-        this._font = CABLES.GLGUI.SDF_FONT_ARIAL;
         this._fontTex = null;
-        // var txt=new Text(this,"H  u  n  d   e   k   u   c   h   e  n");
     }
 
     get rectDrawer() { return this._rectDrawer; }
@@ -22,11 +20,10 @@ export default class GlTextWriter
     {
         if (!this._fontTex)
         {
-            this._fontTex = CGL.Texture.load(this._cgl, "/ui/img/worksans-regular_int.png", // sdf_font_arial.png
-                () =>
-                {
-                    this._rectDrawer.setAllTexture(this._fontTex, true);
-                }, { "flip": false, "filter": CGL.Texture.FILTER_LINEAR });
+            this._fontTex = CGL.Texture.load(this._cgl, "/ui/img/worksans-regular_int.png", () =>
+            {
+                this._rectDrawer.setAllTexture(this._fontTex, true);
+            }, { "flip": false, "filter": CGL.Texture.FILTER_LINEAR });
         }
 
         this._rectDrawer.render(resX, resY, scrollX, scrollY, zoom);
