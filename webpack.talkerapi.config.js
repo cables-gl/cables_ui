213f9f5bc375bd4a619399092330584305c54e9a
Stephan Maihöfer <stephan@undev.de>
2024-03-14 13:51:41 +0100

    add webpack config

---
+++ webpack.talkerapi.config.js
@@ -0,0 +1,29 @@
+import path from "path";
+import webpack from "webpack";
+
+export default (isLiveBuild, buildInfo) =>
+{
+    const __dirname = new URL(".", import.meta.url).pathname;
+    return {
+        "mode": isLiveBuild ? "production" : "development",
+        "entry": [
+            path.join(__dirname, "src-talkerapi", "talkerapi.js"),
+        ],
+        "devtool": isLiveBuild ? "source-map" : "eval-cheap-module-source-map",
+        "output": {
+            "path": path.join(__dirname, "dist/js"),
+            "filename": "talkerapi.js"
+        },
+        "stats": isLiveBuild,
+        "optimization": { "minimize": isLiveBuild },
+        "externals": ["CABLES"],
+        "resolve": {
+            "extensions": [".json", ".js"],
+        },
+        "plugins": [
+            new webpack.DefinePlugin({
+                "window.BUILD_INFO": JSON.stringify(buildInfo)
+            })
+        ]
+    };
+};
