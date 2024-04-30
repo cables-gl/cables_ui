ab2a22d2b8ce88bc1a1060b4a7684956607b782d
pandrr <tom@tomkom.net>
2024-04-30 16:19:30 +0200

    fix npe

--- src/ui/components/tabs/tab_manage_op.js
+++ src/ui/components/tabs/tab_manage_op.js
@@ -171,8 +171,7 @@ export default class ManageOp
 
                     const html = getHandleBarHtml("tab_manage_op",
                         {
-
-                            "url": CABLES.sandbox.getCablesUrl(),
+                            "url": CABLES.platform.getCablesUrl(),
                             "opLayoutSvg": gui.opDocs.getLayoutSvg(opName),
                             "opid": opDoc.id,
                             "opname": opDoc.name,
