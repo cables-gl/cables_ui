615f2f7b053c884666edc1ec3f9e4a0e912d2728
Stephan Maihöfer <stephan@undev.de>
2024-07-16 18:44:55 +0200

    opdirs tab

--- src/ui/components/tabs/tab_standaloneopdirs.js
+++ src/ui/components/tabs/tab_standaloneopdirs.js
@@ -1,3 +1,4 @@
+import { helper } from "cables-shared-client";
 import Tab from "../../elements/tabpanel/tab.js";
 import { getHandleBarHtml } from "../../utils/handlebars.js";
 
@@ -12,9 +13,32 @@ export default class StandaloneOpDirs
         tabs.addTab(this._tab, true);
         this.show();
 
-        CABLESUILOADER.talkerAPI.addEventListener("addedProjectOpDir", (err, res) =>
+        const listenerId = CABLESUILOADER.talkerAPI.addEventListener("projectOpDirsChanged", (err, res) =>
         {
             this.show();
+            CABLESUILOADER.talkerAPI.send("getOpDocsAll", { "projectId": gui.patchId }, (_err, _data) =>
+            {
+                if (_err)
+                {
+                    console.error("preloading error", _err);
+                }
+                else
+                {
+                    if (gui.opDocs)
+                    {
+                        gui.opDocs.addOpDocs(_data.opDocs);
+                    }
+                    gui.opSelect().reload();
+                }
+            }, (response) =>
+            {
+                console.error("preloading error", response);
+            });
+        });
+        this._tab.on("onActivate", this.show);
+        this._tab.on("close", () =>
+        {
+            CABLESUILOADER.talkerAPI.removeEventListener(listenerId);
         });
     }
 
@@ -27,7 +51,46 @@ export default class StandaloneOpDirs
                 const html = getHandleBarHtml("tab_standalone_opdirs", { "dirs": r.data });
                 this._tab.html(html);
 
-                new Sortable(ele.byId("dirlist"), {
+                const listEle = ele.byId("dirlist");
+                const infoBlock = listEle.querySelector(".highlightBlock");
+                const saveButton = this._tab.contentEle.querySelector("#saveProjectOpDirOrder");
+                if (saveButton)
+                {
+                    saveButton.addEventListener("click", () =>
+                    {
+                        ele.hide(infoBlock);
+                        infoBlock.classList.add("info");
+                        infoBlock.classList.remove("error");
+                        const order = [];
+                        const dirs = listEle.querySelectorAll("[data-dir]");
+                        dirs.forEach((dirEle) =>
+                        {
+                            order.push(dirEle.dataset.dir);
+                        });
+                        CABLESUILOADER.talkerAPI.send("saveProjectOpDirOrder", order, (orderErr, orderRes) =>
+                        {
+                            if (orderRes && orderRes.success)
+                            {
+                                infoBlock.innerHTML = "Saved, please reload the patch to see the changes";
+                                saveButton.innerHTML = "Reload";
+                                ele.show(infoBlock);
+                                saveButton.addEventListener("click", () =>
+                                {
+                                    window.location.reload();
+                                });
+                            }
+                            else
+                            {
+                                infoBlock.classList.remove("info");
+                                infoBlock.classList.add("error");
+                                infoBlock.innerHTML = orderErr;
+                                ele.show(infoBlock);
+                            }
+                        });
+                    });
+                }
+
+                new Sortable(listEle, {
                     "animation": 150,
                     "handle": ".handle",
                     "ghostClass": "ghost",
