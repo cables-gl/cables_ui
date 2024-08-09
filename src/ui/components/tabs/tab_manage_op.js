d2d59b3c61a11dbe9a66b5d14217d4975a4d9758
Stephan Maihöfer <stephan@undev.de>
2024-08-09 16:11:12 +0200

    nonmovable opdirs in standalone

--- src/ui/components/tabs/tab_manage_op.js
+++ src/ui/components/tabs/tab_manage_op.js
@@ -154,8 +154,6 @@ export default class ManageOp
                         });
                     });
 
-                    console.log("opDoc", opDoc);
-
                     const html = getHandleBarHtml("tab_manage_op",
                         {
                             "url": CABLES.platform.getCablesUrl(),
