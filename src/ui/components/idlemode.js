7198c95c0535cf62045ce7205dd66089d4b9132f
pandrr <tom@tomkom.net>
2024-11-18 12:09:12 +0100

    idlemode

--- src/ui/components/idlemode.js
+++ src/ui/components/idlemode.js
@@ -18,7 +18,7 @@ function startIdleMode()
 
     if (!CABLES.UI.loaded || !window.gui) return;
     if (idling) return;
-    if (CABLES.UI.userSettings.get("noidlemode")) return;
+    if (!CABLES.UI.userSettings.get("idlemode")) return;
     if (gui.socket && gui.socket.inMultiplayerSession) return;
 
     const wasActiveSeconds = (performance.now() - activeModeStart) / 1000;
