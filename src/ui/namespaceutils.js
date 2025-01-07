f732bdbdd9f0b81281ae6d10d42b2372e62b16b5
pandrr <tom@tomkom.net>
2025-01-07 15:00:46 +0100

    opnameutils

--- src/ui/namespaceutils.js
+++ src/ui/namespaceutils.js
@@ -2,13 +2,13 @@ import defaultOps from "./defaultops.js";
 
 export default class namespace {}
 
-namespace.getNamespaceClassName = function (opName)
+namespace.getNamespaceClassName = (opName) =>
 {
     const opNameParts = opName.split(".");
     return "nsColor_" + opNameParts[0] + "_" + opNameParts[1];
 };
 
-namespace.getNamespace = function (opname)
+namespace.getNamespace = (opname) =>
 {
     if (!opname) return "";
     const parts = opname.split(".");
@@ -16,57 +16,57 @@ namespace.getNamespace = function (opname)
     return parts.join(".") + ".";
 };
 
-namespace.isDevOp = function (opname)
+namespace.isDevOp = (opname) =>
 {
     return opname && opname.includes(".Dev.");
 };
 
-namespace.isUserOp = function (opname)
+namespace.isUserOp = (opname) =>
 {
     return opname && opname.startsWith(defaultOps.prefixes.userOp);
 };
 
-namespace.isCurrentUserOp = function (opname)
+namespace.isCurrentUserOp = (opname) =>
 {
     return namespace.isUserOpOfUser(opname, gui.user.usernameLowercase);
 };
 
-namespace.isUserOpOfUser = function (opname, userNameLowercase)
+namespace.isUserOpOfUser = (opname, userNameLowercase) =>
 {
     return opname && opname.startsWith(defaultOps.prefixes.userOp + userNameLowercase);
 };
 
-namespace.isDeprecatedOp = function (opname)
+namespace.isDeprecatedOp = (opname) =>
 {
     return opname && opname.includes(".Deprecated.");
 };
 
-namespace.isExtensionOp = function (opname)
+namespace.isExtensionOp = (opname) =>
 {
     return opname && opname.startsWith(defaultOps.prefixes.extensionOp);
 };
 
-namespace.isCoreOp = function (opname)
+namespace.isCoreOp = (opname) =>
 {
     return !namespace.isNonCoreOp(opname);
 };
 
-namespace.isNonCoreOp = function (opname)
+namespace.isNonCoreOp = (opname) =>
 {
     return namespace.isUserOp(opname) || namespace.isExtensionOp(opname) || namespace.isTeamOp(opname) || namespace.isPatchOp(opname);
 };
 
-namespace.isPrivateOp = function (opname)
+namespace.isPrivateOp = (opname) =>
 {
     return namespace.isTeamOp(opname) || namespace.isPatchOp(opname) || namespace.isUserOp(opname);
 };
 
-namespace.isPatchOp = function (opname)
+namespace.isPatchOp = (opname) =>
 {
     return opname && opname.indexOf(defaultOps.prefixes.patchOp) === 0;
 };
 
-namespace.isExtension = function (opname)
+namespace.isExtension = (opname) =>
 {
     if (!opname) return false;
     if (!opname.startsWith(defaultOps.prefixes.extensionOp)) return false;
@@ -75,17 +75,17 @@ namespace.isExtension = function (opname)
     return parts.length < 5;
 };
 
-namespace.isCollection = function (opname)
+namespace.isCollection = (opname) =>
 {
     return opname && (namespace.isExtension(opname) || namespace.isTeamNamespace(opname));
 };
 
-namespace.isTeamOp = function (opname)
+namespace.isTeamOp = (opname) =>
 {
     return opname && opname.startsWith(defaultOps.prefixes.teamOp);
 };
 
-namespace.isTeamNamespace = function (opname)
+namespace.isTeamNamespace = (opname) =>
 {
     if (!opname) return false;
     if (!opname.startsWith(defaultOps.prefixes.teamOp)) return false;
