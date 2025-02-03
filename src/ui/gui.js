b0ba191784759b283f55f1984efa5293d6bc12f6
Stephan Maihöfer <stephan@undev.de>
2025-02-03 16:21:56 +0100

    rename

--- src/ui/gui.js
+++ src/ui/gui.js
@@ -2196,7 +2196,7 @@ export default class Gui extends Events
     setUser(u)
     {
         this.user = u;
-        if (this.user.isPatron) ele.hide(ele.byId("nav_support"));
+        if (this.user.isSupporter) ele.hide(ele.byId("nav_support"));
     }
 
     initCoreListeners()
