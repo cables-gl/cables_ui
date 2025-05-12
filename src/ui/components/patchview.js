9e4ae722f5753d72e9e89e200f9047ef964e2765
pandrr <tom@tomkom.net>
2025-05-12 15:50:55 +0200

    canvas resize

--- src/ui/components/patchview.js
+++ src/ui/components/patchview.js
@@ -121,6 +121,7 @@ export default class PatchView extends Events
         const outerOp = gui.corePatch().getSubPatchOuterOp(subPatchId);
         gui.patchView.setCurrentSubPatch(outerOp.uiAttribs.subPatch);
         console.log("lala", outerOp.opId);
+
         gui.patchView.centerSelectOp(outerOp.opId);
     }
 
