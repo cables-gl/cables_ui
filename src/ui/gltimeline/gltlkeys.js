d244b2e56c257da3b35f62f7cf462ca96a8cfcbc
pandrr <tom@tomkom.net>
2025-10-01 11:00:40 +0200

    drag

--- src/ui/gltimeline/gltlkeys.js
+++ src/ui/gltimeline/gltlkeys.js
@@ -611,13 +611,14 @@ export class glTlKeys extends Events
 
                 keyRect.on(GlRect.EVENT_DRAGSTART, (_rect, _x, _y, button, e) =>
                 {
-                    console.log("key startdrag");
+                    console.log("key startdrag1", glTlKeys.dragStarted);
                     if (this.#glTl.isSelecting()) return;
                     glTlKeys.dragStartX = e.offsetX;
                     glTlKeys.dragStartY = e.offsetY;
                     this.#glTl.predragSelectedKeys();
                     if (button == 1 && !glTlKeys.dragStarted)
                     {
+                        console.log("yaaaaaaaaaaa");
                         oldValues = this.#glTl.serializeSelectedKeys();
                         glTlKeys.dragStarted = true;
                         glTlKeys.startDragTime = this.#glTl.view.pixelToTime(e.offsetX);
