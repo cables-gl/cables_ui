2e970e9e62493f1d431af6c837282a61223cae58
pandrr <tom@tomkom.net>
2025-11-23 18:13:34 +0100

    ignore readonly/muted keys when changing... /cables-gl/cables/issues/7733

--- src/ui/gltimeline/tllinetitle.js
+++ src/ui/gltimeline/tllinetitle.js
@@ -325,6 +325,7 @@ export class TlTitle extends Events
                 (e) =>
                 {
                     this.#port.animMuted = !this.#port.animMuted;
+                    this.#port.anim.setUiAttribs({ "muted": this.#port.animMuted });
                     this.#port.emitEvent("animLineUpdate");
 
                     this.updateIcons();
