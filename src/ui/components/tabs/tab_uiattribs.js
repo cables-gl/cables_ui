c4d43f9d3a9f655b440c4ec8898d63b9af7a429a
pandrr <tom@tomkom.net>
2025-12-07 11:52:55 +0100

    gradient alpha

--- src/ui/components/tabs/tab_uiattribs.js
+++ src/ui/components/tabs/tab_uiattribs.js
@@ -13,22 +13,26 @@ import { editorSession } from "../../elements/tabpanel/editor_session.js";
 export default class OpWatchUiAttribs extends Events
 {
     static TABSESSION_NAME = "watchuiattr";
+    #tabs;
+    #tab;
+    #id = "hljs" + utils.uuid();
+    #op = null;
 
+    /**
+     * @param {import("../../elements/tabpanel/tabpanel.js").default} tabs
+     */
     constructor(tabs)
     {
         super();
-        this._tabs = tabs;
-        this._tab = new CABLES.UI.Tab("Op UiAttribs", { "icon": "op", "infotext": "tab_uiattribs", "padding": true, "singleton": "true", });
-        this._tabs.addTab(this._tab, true);
-
-        this._id = "hljs" + utils.uuid();
-
-        this._op = null;
+        this.#tabs = tabs;
+        this.#tab = new CABLES.UI.Tab("Op UiAttribs", { "icon": "op", "infotext": "tab_uiattribs", "padding": true, "singleton": "true", });
+        this.#tabs.addTab(this.#tab, true);
         this.rebuildHtml();
         this.setOp(gui.opParams.op);
+
         editorSession.rememberOpenEditor(OpWatchUiAttribs.TABSESSION_NAME, "profiler", { }, true);
 
-        this._tab.on("close", () =>
+        this.#tab.on("close", () =>
         {
             editorSession.remove(OpWatchUiAttribs.TABSESSION_NAME, "profiler");
         });
@@ -39,6 +43,9 @@ export default class OpWatchUiAttribs extends Events
         });
     }
 
+    /**
+     * @param {Op} op
+     */
     setOp(op)
     {
         if (this._op == op) return;
@@ -76,30 +83,30 @@ export default class OpWatchUiAttribs extends Events
         {
             let html = "<div class=\"tabContentScrollContainer\">";
             html += "<hr/><h2>Op: " + this._op.name + " </h2>";
-            html += "<code ><pre id=\"" + this._id + "\" class=\"hljs language-json\">" + JSON.stringify(this._sortObject(this._op.uiAttribs), false, 4) + "</code></pre>";
+            html += "<code ><pre id=\"" + this.#id + "\" class=\"hljs language-json\">" + JSON.stringify(this._sortObject(this._op.uiAttribs), null, 4) + "</code></pre>";
 
             for (let i = 0; i < this._op.portsIn.length; i++)
             {
                 html += "<hr/><h3>Input Port: " + this._op.portsIn[i].name + " </h3>";
-                html += "<code ><pre id=\"" + this._id + "\" class=\"hljs language-json\">" + JSON.stringify(this._sortObject(this._op.portsIn[i].uiAttribs), false, 4) + "</code></pre>";
+                html += "<code ><pre id=\"" + this.#id + "\" class=\"hljs language-json\">" + JSON.stringify(this._sortObject(this._op.portsIn[i].uiAttribs), null, 4) + "</code></pre>";
             }
 
             for (let i = 0; i < this._op.portsOut.length; i++)
             {
                 html += "<hr/><h3>Output Port: " + this._op.portsOut[i].name + " </h3>";
-                html += "<code ><pre id=\"" + this._id + "\" class=\"hljs language-json\">" + JSON.stringify(this._sortObject(this._op.portsOut[i].uiAttribs), false, 4) + "</code></pre>";
+                html += "<code ><pre id=\"" + this.#id + "\" class=\"hljs language-json\">" + JSON.stringify(this._sortObject(this._op.portsOut[i].uiAttribs), null, 4) + "</code></pre>";
             }
 
             html += "</div>";
 
-            this._tab.html(html);
+            this.#tab.html(html);
         }
         else
         {
-            this._tab.html("please select op");
+            this.#tab.html("please select op");
         }
 
-        const el = ele.byId(this._id);
+        const el = ele.byId(this.#id);
         // hljs.highlightAuto();
 
         if (el)
