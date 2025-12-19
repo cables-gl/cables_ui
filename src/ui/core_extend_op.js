e3e38b388fc4f6bcc4e0fdcd61f50dd02e030981
pandrr <tom@tomkom.net>
2025-12-19 11:20:16 +0100

    fix dupe portname detection...2

--- src/ui/core_extend_op.js
+++ src/ui/core_extend_op.js
@@ -39,11 +39,18 @@ class UiOp extends Op
             {
                 for (let j = 0; j < this.portsOut.length; j++)
                     if (this.portsIn[i] != this.portsOut[j] && this.portsIn[i].name == this.portsOut[j].name)
+                    {
                         this.setUiError("dupeport", "Duplicate Port name: " + this.portsOut[j].name + ". Must be unique!", 2);
+                        console.log("text", this.portsOut[j], this.portsIn[i]);
+                    }
 
                 for (let j = 0; j < this.portsIn.length; j++)
                     if (this.portsIn[i] != this.portsIn[j] && this.portsIn[i].name == this.portsIn[j].name)
+                    {
                         this.setUiError("dupeport", "Duplicate Port name: " + this.portsIn[j].name + ". Must be unique!", 2);
+                        console.log("text2", this.portsIn[j], this.portsIn[i]);
+
+                    }
             }
         });
     }
