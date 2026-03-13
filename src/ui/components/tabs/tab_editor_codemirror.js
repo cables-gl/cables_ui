19100dcd75dda2655f617107b1557cf063ce4c93
pandrr <tom@tomkom.net>
2026-03-13 18:36:09 +0100

    codemirror snippets, syntax check, tab kny

--- src/ui/components/tabs/tab_editor_codemirror.js
+++ src/ui/components/tabs/tab_editor_codemirror.js
@@ -1,14 +1,18 @@
 import { Events, Logger, ele, TalkerAPI } from "cables-shared-client";
-import { EditorView, highlightActiveLineGutter, highlightActiveLine, ViewPlugin, lineNumbers } from "@codemirror/view";
+import { linter, lintGutter } from "@codemirror/lint";
+import { EditorView, highlightActiveLineGutter, highlightActiveLine, ViewPlugin, lineNumbers, keymap } from "@codemirror/view";
 import { Transaction, Extension, EditorState } from "@codemirror/state";
 import { helix, commands } from "codemirror-helix";
+import { syntaxTree } from "@codemirror/language";
 import { autocompletion } from "@codemirror/autocomplete";
 import { jsonLanguage } from "@codemirror/lang-json";
 import { markdown } from "@codemirror/lang-markdown";
-import { javascript } from "@codemirror/lang-javascript";
+import { javascript, javascriptLanguage } from "@codemirror/lang-javascript";
 import { oneDark } from "@codemirror/theme-one-dark";
 // import { basicDark } from 'cm6-theme-basic-dark'
 // import { tokyoNightStorm } from "@fsegurai/codemirror-theme-tokyo-night-storm"
+import { indentWithTab } from "@codemirror/commands";
+import { snippetCompletion } from "@codemirror/autocomplete"; // Built-in function
 import { createOpDocButton } from "../editor.js";
 import { userSettings } from "../usersettings.js";
 import { contextMenu } from "../../elements/contextmenu.js";
@@ -194,6 +198,25 @@ export default class EditorTabCodemirror extends Events
                     "config": { "editor.cursor-shape.insert": "bar" },
                 }
             ));
+        const syntaxErrorLinter = linter((view) =>
+        {
+            const diagnostics = [];
+            syntaxTree(view.state).iterate({
+                enter(node)
+                {
+                    if (node.type.isError)
+                    {
+                        diagnostics.push({
+                            "from": node.from,
+                            "to": node.to,
+                            "severity": "error",
+                            "message": "Syntax error",
+                        });
+                    }
+                },
+            });
+            return diagnostics;
+        });
 
         // extensions.push(customCommands);
         extensions.push(highlightActiveLine());
@@ -202,7 +225,20 @@ export default class EditorTabCodemirror extends Events
         extensions.push(lineNumbers());
         extensions.push(javascript());
         extensions.push(autocompletion());
-        // extensions.push(snippet(snippets));
+        extensions.push(keymap.of([indentWithTab]));
+
+        extensions.push(lintGutter());
+        extensions.push(syntaxErrorLinter);
+
+        extensions.push(
+            javascriptLanguage.data.of({
+                "autocomplete": this.getSnippets()
+            }),
+            autocompletion()
+        );
+
+        // const extensions = [
+        // ];        // extensions.push(snippet(snippets));
 
         this.cmView = new EditorView(
             {
@@ -219,5 +255,206 @@ export default class EditorTabCodemirror extends Events
     setInactive()
     {
         console.log("codemirror setinactive not implemented.");
+    }
+
+    getSnippets()
+    {
+        return [
+
+            snippetCompletion("setUiError = function (\"${1:id}\",\"${1:message}\")",
+                {
+                    "label": "inTriggerButton"
+                }),
+
+            snippetCompletion("inTriggerButton(\"${1:name}\")",
+                {
+                    "label": "inTriggerButton"
+                }),
+
+            snippetCompletion("inTrigger(\"${1:name}\")",
+                {
+                    "label": "inTrigger"
+                }),
+
+            snippetCompletion("inMultiPort(\"#1\", CABLES.OP_PORT_TYPE_NUMBER)",
+                {
+                    "label": "inMultiPort"
+                }),
+
+            snippetCompletion("outTrigger(\"${1:name}\")",
+                {
+                    "label": "outTrigger"
+                }),
+
+            snippetCompletion("inBool(\"${1:name}\",${2:false})",
+                {
+                    "label": "inBool"
+                }),
+
+            snippetCompletion("inInt(\"${1:name}\",${2:0})",
+                {
+                    "label": "inInt"
+                }),
+
+            snippetCompletion("inFloatSlider(\"${1:name}\",${2:0})",
+                {
+                    "label": "inFloatSlider"
+                }),
+
+            snippetCompletion("inFloat(\"${1:name}\",${2:0})",
+                {
+                    "label": "inFloat"
+                }),
+
+            snippetCompletion("inDropDown(\"${1:name}\",\${2:[\"option a\",\"option b\"]}\)",
+                {
+                    "label": "inDropDown"
+                }),
+
+            snippetCompletion("inSwitch(\"${1:name}\",\${2:[\"option a\",\"option b\"]}\,\${3:\"default\"}\)",
+                {
+                    "label": "inSwitch"
+                }),
+
+            snippetCompletion("inStringEditor(\"${1:name}\",\"${2:default}\",\"${3:syntax}\")",
+                {
+                    "label": "inStringEditor"
+                }),
+
+            snippetCompletion("inString(\"${1:name}\",\"${2:default}\")",
+                {
+                    "label": "inString"
+                }),
+
+            snippetCompletion("inObject(\"${1:name}\")",
+                {
+                    "label": "inObject"
+                }),
+
+            snippetCompletion("inTexture(\"${1:name}\")",
+                {
+                    "label": "inTexture"
+                }),
+
+            snippetCompletion("inArray(\"${1:name}\")",
+                {
+                    "label": "inArray"
+                }),
+
+            snippetCompletion("inUrl(\"${1:name}\")",
+                {
+                    "label": "inUrl"
+                }),
+
+            snippetCompletion("outNumber(\"${1:name}\")",
+                {
+                    "label": "outNumber"
+                }),
+
+            snippetCompletion("outBoolNum(\"${1:name}\")",
+                {
+                    "label": "outBoolNum"
+                }),
+
+            snippetCompletion("outString(\"${1:name}\")",
+                {
+                    "label": "outString"
+                }),
+
+            snippetCompletion("outObject(\"${1:name}\")",
+                {
+                    "label": "outObject"
+                }),
+
+            snippetCompletion("outArray(\"${1:name}\")",
+                {
+                    "label": "outArray"
+                }),
+
+            snippetCompletion("outTexture(\"${1:name}\")",
+                {
+                    "label": "outTexture"
+                }),
+
+            snippetCompletion("CABLES.map(${1:name})",
+                {
+                    "label": "CABLES.map"
+                }),
+
+            snippetCompletion("console.log(\"${1:text}\");",
+                {
+                    "label": "console.log"
+                }),
+
+            snippetCompletion("op.setPortGroup(\"${1:name}\",[${2:port},${3:port}]);",
+                {
+                    "label": "op.setPortGroup"
+                }),
+
+            snippetCompletion("CABLES.map(${1:value},${2:oldMin},${3:oldMax},${4:newMin},${5:newMax});",
+                {
+                    "label": "CABLES.map"
+                }),
+
+            snippetCompletion("op.toWorkPortsNeedToBeLinked(${1:port1},${2:port2});",
+                {
+                    "label": "op.toWorkPortsNeedToBeLinked"
+                }),
+
+            snippetCompletion("op.toWorkPortsNeedsString(${1:port1},${2:port2});",
+                {
+                    "label": "op.toWorkPortsNeedsString"
+                }),
+
+            snippetCompletion("vec3.create();",
+                {
+                    "label": "vec3.create"
+                }),
+
+            snippetCompletion("vec3.set(${1:out},${2:x},${3:y},${4:z});",
+                {
+                    "label": "vec3.set(out, x, y, z)"
+                }),
+
+            snippetCompletion("mat4.create();",
+                {
+                    "label": "mat4.create"
+                }),
+
+            snippetCompletion("mat4.identity();",
+                {
+                    "label": "mat4.identity"
+                }),
+
+            snippetCompletion("mat4.translate(${1:out},${2:a},${3:v});",
+                {
+                    "label": "mat4.translate(out,a,v);"
+                }),
+
+            snippetCompletion("CGL.Texture.PFORMATSTR_RGBA32F",
+                {
+                    "label": "CGL.Texture.PFORMATSTR_RGBA32F"
+                }),
+
+            snippetCompletion("CGL.Texture.PFORMATSTR_RGBA8UB",
+                {
+                    "label": "CGL.Texture.PFORMATSTR_RGBA8UB"
+                }),
+
+            snippetCompletion("CGL.Texture.PIXELFORMATS",
+                {
+                    "label": "CGL.Texture.PIXELFORMATS"
+                }),
+
+            snippetCompletion("op.setUiError(\"id\", \"text or null\");",
+                {
+                    "label": "op.setUiError"
+                }),
+
+            snippetCompletion("op.patch.isEditorMode()",
+                {
+                    "label": "op.patch.isEditorMode"
+                }),
+        ];
     }
 }
