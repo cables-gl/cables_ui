9a049d5c1afa9bf947fe73e8fd312f900a081f98
pandrr <tom@tomkom.net>
2026-09-02 09:47:40 +0200

    nodoc

--- src/ui/components/patchview.js
+++ src/ui/components/patchview.js
@@ -3134,7 +3134,7 @@ export default class PatchView extends Events
 
                 for (let i = 0; i < ops.length; i++)
                 {
-                    const doc = this.getOpDocByName(defaultOps.defaultOpNames[i]);
+                    const doc = gui.opDocs.getOpDocByName(defaultOps.defaultOpNames[i]);
                     if (!doc)
                     {
                         ops[i].setUiError("nodoc", "nodoc?");
