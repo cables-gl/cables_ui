d0d43870a027daf72b0cb34ff7effe85c5624ff9
pandrr <tom@tomkom.net>
2026-09-09 16:44:03 +0200

    clean

--- src/ui/glpatch/patchapi.js
+++ src/ui/glpatch/patchapi.js
@@ -43,7 +43,7 @@ export default class GlPatchAPI
         let i = 0;
         for (i = 0; i < this._patch.ops.length; i++)
         {
-            const op = this._patch.ops[i];
+            const op = /** @type {UiOp} */ this._patch.ops[i];
             this._glPatch.addOp(op);
         }
 
@@ -293,7 +293,7 @@ export default class GlPatchAPI
     }
 
     /**
-     * @param {Op} op
+     * @param {UiOp} op
      * @param {boolean} fromDeserialize
      */
     _onAddOp(op, fromDeserialize)
